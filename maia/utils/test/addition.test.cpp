c13119e4376065eb6ddc432d026b987b5fd35776
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-04 13:15:53 +0200

    [CMake] Add python test

--- maia/utils/test/addition.test.cpp
+++ maia/utils/test/addition.test.cpp
@@ -2,7 +2,7 @@
 #include "maia/utils/addition.hpp"
 
 
-MPI_TEST_CASE("[1p] block_to_part",1) {
+MPI_TEST_CASE("[1p] first test",1) {
   auto res = add(1, 2);
   CHECK(res == 3);
   MPI_CHECK(0, res == 3);
---
+++ test/maia_python_unit_tests.py
@@ -0,0 +1,16 @@
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 4
+    assert inc(4) == 5
+    assert inc(5) == 6
+    # assert inc(6) == 8
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 4
+    assert inc(4) == 5
+    assert inc(5) == 6
+    # assert inc(6) == 8
