ec7276de1c57e221dc90c4ad4291ef3ef50b5d17
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-04 18:37:43 +0200

    [CMake] Setup generate scenarii

--- test/maia_python_unit_tests.py
+++ test/maia_python_unit_tests.py
@@ -1,4 +1,5 @@
 import pytest
+import itertools as ITT
 
 def inc(x):
     return x + 1
@@ -9,6 +10,7 @@ def test_answer():
     assert inc(5) == 6
     # assert inc(6) == 8
 
+# --------------------------------------------------------------------------
 def assert_mpi(comm, rank, cond ):
   if(comm.rank == rank):
     print(cond)
@@ -16,13 +18,13 @@ def assert_mpi(comm, rank, cond ):
   else:
     pass
 
-
+# --------------------------------------------------------------------------
 def test_first_step():
   """
   """
-  print("test_first_step")
+  # print("test_first_step")
   from maia.utils import boundary_algorihms as MUB
-  print(MUB.__file__)
+  # print(MUB.__file__)
   from maia.utils import first_step as MUF
 
   zone_u1 = MUF.zone_unstructured("zone_name", 1)
@@ -33,6 +35,7 @@ def test_first_step():
   assert(zone_s1.global_id == 2          );
   assert(zone_s1.name      == "cartesian");
 
+# --------------------------------------------------------------------------
 def test_mpi():
   """
   """
@@ -43,15 +46,41 @@ def test_mpi():
   assert_mpi(comm, 0, comm.rank == 0)
   assert_mpi(comm, 1, comm.rank == 1)
 
+# --------------------------------------------------------------------------
 @pytest.mark.mpi
 def test_size():
   from mpi4py import MPI
   comm = MPI.COMM_WORLD
   assert comm.size > 0
 
+# --------------------------------------------------------------------------
 @pytest.mark.mpi(min_size=2)
 def test_size():
   from mpi4py import MPI
   comm = MPI.COMM_WORLD
   print("hello", comm.size, comm.rank)
   assert comm.size >= 2
+
+# --------------------------------------------------------------------------
+# def pytest_generate_tests(metafunc):
+#   idlist = []
+#   argvalues = []
+#   for scenario in metafunc.cls.scenarios:
+#       idlist.append(scenario[0])
+#       items = scenario[1].items()
+#       argnames = [x[0] for x in items]
+#       argvalues.append([x[1] for x in items])
+#   metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
+
+
+# scenario1 = ("basic"   , {"attribute": "value" })
+# scenario2 = ("advanced", {"attribute": "value2"})
+
+# class TestSampleWithScenarios:
+#   scenarios = [scenario1, scenario2]
+
+#   def test_demo1(self, attribute):
+#     assert isinstance(attribute, str)
+
+#   def test_demo2(self, attribute):
+#     assert isinstance(attribute, str)
