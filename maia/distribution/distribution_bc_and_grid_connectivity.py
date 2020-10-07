a7221a09085b271f1114fc4d9b0c01cb555b4313
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-07 17:09:48 +0200

    Add files

---
+++ maia/distribution/distribution_bc_and_grid_connectivity.py
@@ -0,0 +1,13 @@
+import Converter.PyTree   as C
+import Converter.Internal as I
+
+def compute_distribution_bc(bc, comm):
+  """
+  """
+  pass
+
+
+def compute_distribution_grid_connectivity(bc, comm):
+  """
+  """
+  pass
