de5e2584b0ac3cb9574ade51f27caf27e6704c5a
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-08 11:38:18 +0200

    Add elemts distrib

--- maia/distribution/distribution_elements.py
+++ maia/distribution/distribution_elements.py
@@ -8,11 +8,13 @@ def compute_elements_distribution(zone, comm):
   """
   """
   zone_type_n = I.getNodeFromType1(zone, 'ZoneType_t')
-  zone_type   = zone_type_n[1].to_string()
+  zone_type   = zone_type_n[1].tostring()
   if(zone_type == b'Structured'):
     return
 
   elmts = I.getNodesFromType1(zone, 'Elements_t')
 
   for elmt in elmts:
-    print(elmt)
+    er = I.getNodeFromName(elmt, 'ElementRange')
+    n_tot_elmt = er[1][1] - er[1][0] + 1
+    create_distribution_node(n_tot_elmt, comm, 'distrib_elmt', elmt)
