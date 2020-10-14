5c988ef10572233daa1b95a5942a69178d75fc84
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-14 15:51:52 +0200

    Setup first window algortihm

--- maia/utils/mpi_scheduler.cpp
+++ maia/utils/mpi_scheduler.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include "std_e/algorithm/distribution.hpp"
 #include "maia/utils/mpi_scheduler.hpp"
+#include "std_e/logging/log.hpp"
+#include "std_e/interval/knot_sequence.hpp"
 
 // --------------------------------------------------------------------------------------
 std::vector<int>
@@ -12,12 +14,15 @@ setup_test_distribution(MPI_Comm&         comm,
   std::vector<int> dtest_proc(1+n_rank);
   std_e::uniform_distribution(begin(dtest_proc), end(dtest_proc), static_cast<int>(n_rank_for_test.size()));
 
-  std::cout << "dtest_proc::";
+  std::string s = "dtest_proc::";
   for(int i = 0; i < n_rank+1; ++i){
-    std::cout << dtest_proc[i] << " ";
+    s += std::to_string(dtest_proc[i]) + " ";
   }
-  std::cout << std::endl;
+  s += "\n";
+  std::cout << s << std::endl;
+  // std_e::log("file", s);
 
+  // return std_e::to_knot_vector<int>(dtest_proc);
   return dtest_proc;
 }
 
@@ -62,13 +67,52 @@ void run_scheduler(MPI_Comm&                                    comm,
   // II/ Setup the distributed rank value for each test
   auto dtest_proc = setup_test_distribution(comm, n_rank_for_test);
 
+  // For each test we store a distributed array that contains the number of rank that can execute the test
+  // Increment by one via MPI_Put
+  std::vector<int> count_rank_for_test(dtest_proc[i_rank+1] - dtest_proc[i_rank], 0);
+  // std::vector<int> list_rank_for_test(dtest_proc[i_rank+1] - dtest_proc[i_rank], 0);
+
+  MPI_Win win_count_rank_for_test;
+  MPI_Win_create(count_rank_for_test.data(),              /* base ptr */
+                 count_rank_for_test.size()*sizeof(int),  /* size     */
+                 sizeof(int),                             /* disp_unit*/
+                 MPI_INFO_NULL,
+                 comm,
+                 &win_count_rank_for_test);
+  MPI_Barrier(comm);
   // III/ Setup
+  std::vector<int> remote_copy_count_rank(2); // []
+  int i_test = 0;
+
   bool last_decision_failed = false; // 0 = false / 1 = true
   if(!last_decision_failed ){
 
-
+    int i_target_rank = std_e::interval_index(i_test, dtest_proc);
+    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, i_target_rank, 0, win_count_rank_for_test);
+    MPI_Get(remote_copy_count_rank.data(),    /* origin_addr     */
+            1,                                /* origin_count    */
+            MPI_INT,                          /* origin_datatype */
+            i_target_rank,                    /* target_rank     */
+            0,                                /* target_disp     */
+            1,                                /* target_count    */
+            MPI_INT,                          /* target_datatype */
+            win_count_rank_for_test);         /* win             */
+    printf("[%i] have value : %i | i_target_rank = %i \n", i_rank, remote_copy_count_rank[0], i_target_rank);
+    if(remote_copy_count_rank[0] < n_rank_for_test[i_test]){
+      remote_copy_count_rank[0] += 1;
+      MPI_Put(remote_copy_count_rank.data(),    /* origin_addr     */
+              1,                                /* origin_count    */
+              MPI_INT,                          /* origin_datatype */
+              i_target_rank,                    /* target_rank     */
+              0,                                /* target_disp     */
+              1,                                /* target_count    */
+              MPI_INT,                          /* target_datatype */
+              win_count_rank_for_test);         /* win             */
+    }
+    MPI_Win_unlock(i_target_rank, win_count_rank_for_test);
   }
 
   // Free
   MPI_Win_free(&win_global_n_rank_available);
+  MPI_Win_free(&win_count_rank_for_test);
 }
