b2942e9ca8586d5f339ed4cb0eeb4730b0831ee1
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-10-16 15:44:44 +0200

    Minor (distribution_function cleaning, 2)

--- maia/distribution/test/test_distribution_function.py
+++ maia/distribution/test/test_distribution_function.py
@@ -4,34 +4,33 @@ from   mpi4py import MPI
 
 import maia.distribution as MID
 
-# TODO SUBCASE or equivalent in pytest?
-class Test_uniform_distribution_at
-def test_uniform_distribution_at(int_type):
-  def test_exact():
-    n_elt  = 15
+class Test_uniform_distribution_at:
+  def test_uniform_distribution_at(int_type):
+    def test_exact():
+      n_elt  = 15
 
-    distib = MID.uniform_distribution_at(n_elt,0,3)
-    assert distib[0]    == 0
-    assert distib[1]    == 6
-    distib = MID.uniform_distribution_at(n_elt,1,3)
-    assert distib[0]    == 5
-    assert distib[1]    == 10
-    distib = MID.uniform_distribution_at(n_elt,2,3)
-    assert distib[0]    == 10
-    assert distib[1]    == 15
+      distib = MID.uniform_distribution_at(n_elt,0,3)
+      assert distib[0]    == 0
+      assert distib[1]    == 6
+      distib = MID.uniform_distribution_at(n_elt,1,3)
+      assert distib[0]    == 5
+      assert distib[1]    == 10
+      distib = MID.uniform_distribution_at(n_elt,2,3)
+      assert distib[0]    == 10
+      assert distib[1]    == 15
 
-  def test_inexact():
-    n_elt  = 17
+    def test_inexact():
+      n_elt  = 17
 
-    distib = MID.uniform_distribution_at(n_elt,0,3)
-    assert distib[0]    == 0
-    assert distib[1]    == 6
-    distib = MID.uniform_distribution_at(n_elt,1,3)
-    assert distib[0]    == 6
-    assert distib[1]    == 12
-    distib = MID.uniform_distribution_at(n_elt,2,3)
-    assert distib[0]    == 12
-    assert distib[1]    == 17
+      distib = MID.uniform_distribution_at(n_elt,0,3)
+      assert distib[0]    == 0
+      assert distib[1]    == 6
+      distib = MID.uniform_distribution_at(n_elt,1,3)
+      assert distib[0]    == 6
+      assert distib[1]    == 12
+      distib = MID.uniform_distribution_at(n_elt,2,3)
+      assert distib[0]    == 12
+      assert distib[1]    == 17
 
 
 #@pytest.mark.mpi(min_size=1)
