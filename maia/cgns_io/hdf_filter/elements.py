5f014a733f5efe843a8fea7f55c623d02c77a6d3
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-23 07:17:01 +0200

    Add files to prepare hdf_filter - prepare elmts

--- maia/cgns_io/hdf_filter/elements.py
+++ maia/cgns_io/hdf_filter/elements.py
@@ -1,3 +1,32 @@
 import Converter.PyTree   as C
 import Converter.Internal as I
 import Converter.Filter   as CFilter
+
+
+def create_std_zone_elements_filter(zone_tree,
+                                    zone_path,
+                                    hdf_filter):
+  """
+  """
+  print("create_std_elements_filter")
+
+  elmts_ini = I.getNodesFromType1(zone_tree, 'Elements_t')
+  for elmt in elmts_ini:
+
+    if(elmt[1][0] == 22 or elmt[1][0] == 23 or elmt[1][0] == 20): # Skip NGon/NFac/Mixed
+      continue
+
+    distrib_ud   = I.getNodeFromName1(elmt      , ':CGNS#Distribution')
+    distrib_elmt = I.getNodeFromName1(distrib_ud, 'Distribution_elmt')[1]
+    dn_elmt      = distrib_elmt[1] - distrib_elmt[0]
+
+    elmt_npe = EU.get_npe_with_element_type_cgns(elmt[1][0])
+
+    DSMMRYElmt = [[0                       ], [1], [dn_elmt*elmt_npe], [1]]
+    DSFILEElmt = [[distrib_elmt[0]*elmt_npe], [1], [dn_elmt*elmt_npe], [1]]
+    DSGLOBElmt = [[distrib_elmt[2]*elmt_npe]]
+    DSFORMElmt = [[0]]
+
+    path = zone_path+"/"+elmt[0]+"/ElementConnectivity"
+    hdf_filter[path] = DSMMRYElmt + DSFILEElmt + DSGLOBElmt + DSFORMElmt
+
