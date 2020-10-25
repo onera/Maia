3ba42cbc2567b46dfeb12cf5863985e9657d5bd0
Bruno Maugars <bruno.maugars@onera.fr>
2020-10-25 09:15:51 +0100

    Recursive loop OK

--- maia/cgns_io/cgns_io_tree.py
+++ maia/cgns_io/cgns_io_tree.py
@@ -2,6 +2,14 @@ import Converter.Internal as     I
 import Converter.PyTree   as     C
 from typing import Dict
 
+def update_tree_with_partial_load_dict(dist_tree, partial_dict_load):
+  """
+  """
+  for path, data in partial_dict_load.items():
+    Node = I.getNodeFromPath(dist_tree, path)
+    Node[1] = data
+
+
 def load_tree_from_filter(filename, dist_tree, comm, hdf_filter):
   """
   """
@@ -10,29 +18,29 @@ def load_tree_from_filter(filename, dist_tree, comm, hdf_filter):
   # hdf_filter_with_func = dict(filter(lambda x: not isinstance(x[1], list), hdf_filter.items() ))
 
   hdf_filter_with_dim  = {key: value for (key, value) in hdf_filter.items() if isinstance(value, list)}
-  hdf_filter_with_func = {key: value for (key, value) in hdf_filter.items() if not isinstance(value, list)}
 
+  # > First load :
   partial_dict_load = C.convertFile2PartialPyTreeFromPath(filename, hdf_filter_with_dim, comm)
 
-  print("hdf_filter_with_dim :: ")
-  for key, val in hdf_filter_with_dim.items():
-    print(key, val)
+  update_tree_with_partial_load_dict(dist_tree, partial_dict_load)
 
-  print("hdf_filter_with_func :: ")
-  for key, val in hdf_filter_with_func.items():
-    print(key, val)
+  hdf_filter_with_func = {key: value for (key, value) in hdf_filter.items() if not isinstance(value, list)}
 
-  for path, data in partial_dict_load.items():
-    Node = I.getNodeFromPath(dist_tree, path)
-    Node[1] = data
+  unlock_at_least_one = True
+  while(len(hdf_filter_with_func) > 0 and unlock_at_least_one ):
+    # Update if you can
+    next_hdf_filter = dict()
+    unlock_at_least_one = False
+    for key, f in hdf_filter_with_func.items():
+      try:
+        f(next_hdf_filter)
+        unlock_at_least_one = True
+      except RuntimeError: # Not ready yet
+        pass
+    partial_dict_load = C.convertFile2PartialPyTreeFromPath(filename, next_hdf_filter, comm)
 
-  # Update
-  next_hdf_filter = dict()
-  for key, f in hdf_filter_with_func.items():
-    f(next_hdf_filter)
+    update_tree_with_partial_load_dict(dist_tree, partial_dict_load)
+    hdf_filter_with_func = {key: value for (key, value) in next_hdf_filter.items() if not isinstance(value, list)}
 
-  partial_dict_load = C.convertFile2PartialPyTreeFromPath(filename, next_hdf_filter, comm)
-
-  for path, data in partial_dict_load.items():
-    Node = I.getNodeFromPath(dist_tree, path)
-    Node[1] = data
+  if(unlock_at_least_one is False):
+    raise RuntimeError("Something strange in the loading process")
