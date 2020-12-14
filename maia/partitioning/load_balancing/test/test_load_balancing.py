27b7d3186c6e70e3f03a2a929c229d39f778bfe6
Julien Coulet <julien.coulet@onera.fr>
2020-12-14 11:17:38 +0100

    Add high level module to balance trees

--- maia/partitioning/load_balancing/test/test_load_balancing.py
+++ maia/partitioning/load_balancing/test/test_load_balancing.py
@@ -4,33 +4,34 @@ from maia.partitioning.load_balancing import single_zone_balancing
 from maia.partitioning.load_balancing import balancing_quality
 from maia.partitioning.load_balancing import utils
 
-def test_balancing_quality():
-  #Single zone
-  repart = np.array([[20,20,20,25,20]], dtype=np.int32) 
-  out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
-  assert np.all(out[:3] == [21, 20, 25])
-  assert np.all(abs(out[3:5] - np.array([0.8944, 0.04259])) < 1E-4)
-  assert np.all(out[5:] == [5,20,25])
+class Test_balancing_quality:
+  def test_single_zone(self):
+    repart = np.array([[20,20,20,25,20]], dtype=np.int32) 
+    out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
+    assert np.all(out[:3] == [21, 20, 25])
+    assert np.all(abs(out[3:5] - np.array([0.8944, 0.04259])) < 1E-4)
+    assert np.all(out[5:] == [5,20,25])
 
-  #Single proc
-  repart = np.array([[100], [20], [300]], dtype=np.int32) 
-  out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
-  assert np.all(out == [420, 420, 420, 0, 0, 3, 20, 300])
+  def test_single_proc(self):
+    repart = np.array([[100], [20], [300]], dtype=np.int32) 
+    out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
+    assert np.all(out == [420, 420, 420, 0, 0, 3, 20, 300])
 
-  #Multiple
-  repart = np.array([[0, 75,  0], 
-                     [75, 0,75]], dtype=np.int32)
-  out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
-  assert np.all(out == [75,75,75,0,0,3,75,75])
+  def test_multiple_A(self):
+    repart = np.array([[0, 75,  0], 
+                       [75, 0,75]], dtype=np.int32)
+    out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
+    assert np.all(out == [75,75,75,0,0,3,75,75])
 
-  repart = np.array([[50, 50], 
-                     [15, 15],
-                     [45,  0],
-                     [ 0, 10]], dtype=np.int32)
-  out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
-  assert np.all(out[:3] == [93, 75, 110])
-  assert np.all(abs(out[3:5] - np.array([12.3794, 0.1331])) < 1E-4)
-  assert np.all(out[5:] == [6,10,50])
+  def test_multiple_B(self):
+    repart = np.array([[50, 50], 
+                       [15, 15],
+                       [45,  0],
+                       [ 0, 10]], dtype=np.int32)
+    out = np.asarray(balancing_quality.compute_balance_and_splits(repart))
+    assert np.all(out[:3] == [93, 75, 110])
+    assert np.all(abs(out[3:5] - np.array([12.3794, 0.1331])) < 1E-4)
+    assert np.all(out[5:] == [6,10,50])
 
 def test_search_match():
   assert utils.search_match([10,20,50,70], 60) == (0,2)
---
+++ maia/partitioning/load_balancing/test/test_setup_partition_weights.py
@@ -0,0 +1,77 @@
+import pytest
+import numpy as np
+from mpi4py import MPI
+
+import Converter.Internal as I
+
+from maia.utils import parse_yaml_cgns
+from maia.partitioning.load_balancing import setup_partition_weights
+
+@pytest.mark.mpi(min_size=3)
+@pytest.mark.parametrize("sub_comm", [3], indirect=['sub_comm'])
+class Test_npart_per_zone_3p:
+  yt = """
+Base0 CGNSBase_t [3,3]:
+  ZoneU1 Zone_t [[1331,1000,0]]:
+  ZoneU2 Zone_t [[216,125,0]]:
+  ZoneS Zone_t [[21,20,0],[21,20,0],[2,1,0]]:
+"""
+  dist_tree = parse_yaml_cgns.to_complete_pytree(yt)
+  def test_one_part(self, sub_comm):
+    if(sub_comm == MPI.COMM_NULL):
+      return
+    zone_to_weights = setup_partition_weights.npart_per_zone(self.dist_tree, sub_comm)
+    for zone in I.getZones(self.dist_tree):
+      assert I.getName(zone) in zone_to_weights
+    for zone, weights in zone_to_weights.items():
+      assert len(weights) == 1
+      assert abs(weights[0] - 1./3.) < 1E-2 #Bad precision due to remainder
+
+  def test_multiple_part(self, sub_comm):
+    if(sub_comm == MPI.COMM_NULL):
+      return
+    n_part = 2 if sub_comm.Get_rank() == 1 else 1
+    zone_to_weights = setup_partition_weights.npart_per_zone(self.dist_tree, sub_comm, n_part)
+    for zone, weights in zone_to_weights.items():
+      assert len(weights) == n_part
+      for weight in weights:
+        assert abs(weight - 0.25) < 1E-2 #Bad precision due to remainder
+
+@pytest.mark.mpi(min_size=2)
+@pytest.mark.parametrize("sub_comm", [2], indirect=['sub_comm'])
+class Test_balance_multizone_tree_2p:
+  yt = """
+Base0 CGNSBase_t [3,3]:
+  ZoneU1 Zone_t [[1331,1000,0]]:
+  ZoneU2 Zone_t [[216,125,0]]:
+  ZoneS Zone_t [[21,20,0],[21,20,0],[2,1,0]]:
+"""
+  dist_tree = parse_yaml_cgns.to_complete_pytree(yt)
+
+  def test_uniform(self, sub_comm):
+    if(sub_comm == MPI.COMM_NULL):
+      return
+    zone_to_weights = setup_partition_weights.balance_multizone_tree(self.dist_tree, 
+        sub_comm, only_uniform=True)
+    if sub_comm.Get_rank() == 0:
+      assert zone_to_weights['ZoneU1'] == [1.0]
+      assert zone_to_weights['ZoneU2'] == [1.0]
+      assert zone_to_weights['ZoneS']  == []
+    elif sub_comm.Get_rank() == 1:
+      assert zone_to_weights['ZoneU1'] == []
+      assert zone_to_weights['ZoneU2'] == []
+      assert zone_to_weights['ZoneS']  == [1.0]
+
+  def test_non_uniform(self, sub_comm):
+    if(sub_comm == MPI.COMM_NULL):
+      return
+    zone_to_weights = setup_partition_weights.balance_multizone_tree(self.dist_tree, sub_comm)
+    if sub_comm.Get_rank() == 0:
+      assert zone_to_weights['ZoneU1'] == [.238]
+      assert zone_to_weights['ZoneU2'] == [1.0]
+      assert zone_to_weights['ZoneS']  == [1.0]
+    elif sub_comm.Get_rank() == 1:
+      assert zone_to_weights['ZoneU1'] == [.762]
+      assert zone_to_weights['ZoneU2'] == []
+      assert zone_to_weights['ZoneS']  == []
+
