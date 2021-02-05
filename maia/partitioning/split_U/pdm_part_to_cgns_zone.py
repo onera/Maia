a5006e1bfe2243f2a2fad329c8011fd337e6af15
Julien Coulet <julien.coulet@onera.fr>
2021-02-05 15:15:03 +0100

    Output element LNGN after part

--- maia/partitioning/split_U/pdm_part_to_cgns_zone.py
+++ maia/partitioning/split_U/pdm_part_to_cgns_zone.py
@@ -190,11 +190,15 @@ def pdm_elmt_to_cgns_elmt(p_zone, d_zone, dims, data):
     I.newDataArray('ElementStartOffset' , data['np_face_vtx_idx'], parent=ngon_n)
     I.newDataArray('ParentElements'     , pe                     , parent=ngon_n)
     I.newPointRange('ElementRange'      , [1, n_face]            , parent=ngon_n)
+    lngn_elmt = I.createUniqueChild(ngon_n, ':CGNS#GlobalNumbering', 'UserDefinedData_t')
+    I.newDataArray('Element', data['np_face_ln_to_gn'], parent=lngn_elmt)
 
     nface_n = I.createUniqueChild(p_zone, 'NFaceElements', 'Elements_t', value=[23,0])
     I.newDataArray('ElementConnectivity', data['np_cell_face']     , parent=nface_n)
     I.newDataArray('ElementStartOffset' , data['np_cell_face_idx'] , parent=nface_n)
     I.newPointRange('ElementRange'      , [n_face+1, n_face+n_cell], parent=nface_n)
+    lngn_elmt = I.createUniqueChild(nface_n, ':CGNS#GlobalNumbering', 'UserDefinedData_t')
+    I.newDataArray('Element', data['np_cell_ln_to_gn'], parent=lngn_elmt)
 
   else:
     elt_section_nodes = I.getNodesFromType(d_zone, "Elements_t")
@@ -209,6 +213,8 @@ def pdm_elmt_to_cgns_elmt(p_zone, d_zone, dims, data):
       I.newDataArray('ElementConnectivity', data['np_elt_vtx'][i_elt]       , parent=elt_n)
       I.newPointRange('ElementRange'      , [n_elt_cum+1, n_elt_cum+n_i_elt], parent=elt_n)
       n_elt_cum += n_i_elt
+      lngn_elmt = I.createUniqueChild(elt_n, ':CGNS#GlobalNumbering', 'UserDefinedData_t')
+      I.newDataArray('Element', data['np_elt_section_ln_to_gn'][i_elt], parent=lngn_elmt)
 
 
 def pdm_part_to_cgns_zone(dist_zone, l_dims, l_data, comm, options):
