cbc37de11a509ff683730c8a56615445d058c6a3
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2021-02-10 20:25:23 +0100

    Minor

--- maia/transform/fsdm_distribution.cpp
+++ maia/transform/fsdm_distribution.cpp
@@ -1,6 +1,7 @@
 #include "maia/transform/fsdm_distribution.hpp"
 #include "cpp_cgns/sids/creation.hpp"
 #include "cpp_cgns/sids/sids.hpp"
+#include "cpp_cgns/tree_manip.hpp"
 #include "maia/utils/parallel/distribution.hpp"
 #include "std_e/buffer/buffer_vector.hpp"
 
@@ -8,6 +9,7 @@
 namespace cgns {
 
 auto add_fsdm_distribution(tree& b, MPI_Comm comm) -> void {
+  STD_E_ASSERT(b.label=="CGNSBase_t");
   auto zs = get_children_by_label(b,"Zone_t");
   if (zs.size()!=1) {
     throw cgns_exception("add_fsdm_distribution (as FSDM) expects only one zone per process");
@@ -19,7 +21,10 @@ auto add_fsdm_distribution(tree& b, MPI_Comm comm) -> void {
   int n_rank = std_e::nb_ranks(comm);
 
   auto n_vtx = VertexSize_U<I4>(z);
-  auto n_ghost_node = get_node_value_by_matching<I4>(z,"GridCoordinates/FSDM#n_ghost")[0];
+  auto n_ghost_node = 0;
+  if (cgns::has_node(z,"GridCoordinates/FSDM#n_ghost")) {
+    n_ghost_node = get_node_value_by_matching<I4>(z,"GridCoordinates/FSDM#n_ghost")[0];
+  }
   I4 n_owned_vtx = n_vtx - n_ghost_node;
   auto vtx_distri = distribution_from_dsizes(n_owned_vtx, comm);
   std_e::buffer_vector<I4> vtx_distri_mem(n_rank+1);
