e263a63b196a96dd9fca3824b930255adcbe5c18
Julien Coulet <julien.coulet@onera.fr>
2021-04-22 10:03:07 +0200

    Simplify connectivity/cngs_to_dmeshnodal

--- maia/connectivity/cgns_to_pdm_dmeshnodal.py
+++ maia/connectivity/cgns_to_pdm_dmeshnodal.py
@@ -1,58 +1,14 @@
-import Converter.PyTree   as C
 import Converter.Internal as I
-import numpy              as NPY
+
+from maia import npy_pdm_gnum_dtype as pdm_gnum_dtype
 
 import maia.sids.sids as SIDS
-import maia.sids.Internal_ext as IE
+from maia.utils import py_utils
 from maia.utils import zone_elements_utils as EZU
+from maia.tree_exchange.dist_to_part.index_exchange import collect_distributed_pl
 
 import Pypdm.Pypdm as PDM
 from maia import npy_pdm_gnum_dtype as npy_pdm_gnum_t
-
-
-def concatenate_bc(zone):
-  """
-  """
-  n_elmt_group = 0
-  for zone_bc in I.getNodesFromType1(zone, 'ZoneBC_t'):
-    bcs = I.getNodesFromType1(zone_bc, 'BC_t')
-    n_elmt_group += len(bcs)
-
-  delmt_bound        = list()
-  delmt_bound_idx    = NPY.empty(n_elmt_group+1, dtype='int32', order='F')
-  delmt_bound_idx[0] = 0
-  for zone_bc in I.getNodesFromType1(zone, 'ZoneBC_t'):
-    bcs = I.getNodesFromType1(zone_bc, 'BC_t')
-    for i_group, bc in enumerate(bcs):
-      pl_n = I.getNodeFromName1(bc, 'PointList')
-      if pl_n is not None:
-        # > Don't use I.getValue which return an int instead of np array if len(PL)=1
-        pl = pl_n[1][0,:]
-      else:
-        pr_n = I.getNodeFromName1(bc, 'PointRange')
-        if(pr_n is not None):
-          pr = I.getValue(pr_n)
-          distrib        = IE.getDistribution(bc, 'Index')
-          pl = NPY.arange(pr[0][0]+distrib[0], pr[0][0]+distrib[1], dtype=pr.dtype)
-        else:
-          pl = NPY.empty(0, dtype='int32', order='F')
-      delmt_bound.append(pl)
-      delmt_bound_idx[i_group+1] = delmt_bound_idx[i_group] + pl.shape[0]
-
-  if n_elmt_group > 0:
-    delmt_bound = NPY.concatenate(delmt_bound,dtype=npy_pdm_gnum_t)
-  else:
-    delmt_bound = None
-
-  #print(n_elmt_group, delmt_bound_idx, delmt_bound)
-
-  # > Holder state
-  pdm_node = I.createUniqueChild(zone, ':CGNS#DMeshNodal#Bnd', 'UserDefinedData_t')
-  I.newDataArray('delmt_bound_idx', delmt_bound_idx, parent=pdm_node)
-  I.newDataArray('delmt_bound'    , delmt_bound    , parent=pdm_node)
-
-  return n_elmt_group, delmt_bound_idx, delmt_bound
-
 
 def cgns_to_pdm(zone, comm):
   """
@@ -60,39 +16,32 @@ def cgns_to_pdm(zone, comm):
   n_vtx  = SIDS.zone_n_vtx (zone)
   n_cell = SIDS.zone_n_cell(zone)
 
-  # dvtx_coord  = NPY.empty(n_vtx*3, dtype='double', order='C')
+  dmesh_nodal = PDM.DistributedMeshNodal(comm, n_vtx, n_cell)
+
+  # dvtx_coord  = np.empty(n_vtx*3, dtype='double', order='C')
+  # dmesh_nodal.set_coordinnates(dvtx_coord)
   # print("TODO REMOVE COORDINATE in generate_ngon_from_std_elements.pyx")
 
-  elmt_by_ascending_gnum = EZU.get_ordered_elements_std(zone)
+  sorted_elmts = EZU.get_ordered_elements_std(zone)
 
-  nb_elemts = 0;
-  for elmt in elmt_by_ascending_gnum:
-    # If NGon / NFac skip
-    elmt_range  = I.getNodeFromName1(elmt, "ElementRange")
-    nb_elemts  += elmt_range[1][1]-elmt_range[1][0]+1
+  # nb_elemts = sum([SIDS.ElementSize(elmt) for elmt in sorted_elmts])
   # assert(n_cell == nb_elemts)
 
-  elmt_by_ascending_gnum_type, elmt_by_ascending_gnum_n_elmt = EZU.collect_pdm_type_and_nelemts(elmt_by_ascending_gnum)
-  sort_elmt_connect = EZU.collect_connectity(elmt_by_ascending_gnum)
+  elmts_pdm_type, elmts_dn = EZU.collect_pdm_type_and_nelemts(sorted_elmts)
+  elmts_connectivities = EZU.collect_connectity(sorted_elmts)
 
-  n_elmt_group, delmt_bound_idx, delmt_bound = concatenate_bc(zone)
+  dmesh_nodal.set_sections(elmts_connectivities, elmts_pdm_type, elmts_dn)
 
-  distributed_mesh_nodal = PDM.DistributedMeshNodal(comm, n_vtx, n_cell)
+  bc_point_lists = collect_distributed_pl(zone, ['ZoneBC_t/BC_t'])
+  delmt_bound_idx, delmt_bound = py_utils.concatenate_point_list(bc_point_lists, pdm_gnum_dtype)
+  n_elmt_group = delmt_bound_idx.shape[0] - 1
 
-  # distributed_mesh_nodal.set_coordinnates(dvtx_coord)
-  distributed_mesh_nodal.set_group_elmt(n_elmt_group, delmt_bound_idx, delmt_bound)
+  #Need an holder to prevent memory delete
+  pdm_node = I.createUniqueChild(zone, ':CGNS#DMeshNodal#Bnd', 'UserDefinedData_t')
+  I.newDataArray('delmt_bound_idx', delmt_bound_idx, parent=pdm_node)
+  I.newDataArray('delmt_bound'    , delmt_bound    , parent=pdm_node)
 
-  # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # print("type(elmt_by_ascending_gnum)       ::", type(elmt_by_ascending_gnum))
-  # print("type(elmt_by_ascending_gnum_type)  ::", type(elmt_by_ascending_gnum_type))
-  # print("type(elmt_by_ascending_gnum_n_elmt)::", type(elmt_by_ascending_gnum_n_elmt))
-  # print("sort_elmt_connect   ::", sort_elmt_connect)
-  # print("elmt_by_ascending_gnum_type  ::", elmt_by_ascending_gnum_type)
-  # print("elmt_by_ascending_gnum_n_elmt::", elmt_by_ascending_gnum_n_elmt)
-  distributed_mesh_nodal.set_sections(sort_elmt_connect,
-                                      elmt_by_ascending_gnum_type,
-                                      elmt_by_ascending_gnum_n_elmt)
-  # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  dmesh_nodal.set_group_elmt(n_elmt_group, delmt_bound_idx, delmt_bound)
 
-  return distributed_mesh_nodal
+  return dmesh_nodal
 
