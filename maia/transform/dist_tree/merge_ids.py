52922c4e1a00b1141333a0d38a7956f35eaff9d6
Julien Coulet <julien.coulet@onera.fr>
2021-05-10 15:26:49 +0200

    New function to merge some elements in a distributed way

---
+++ maia/transform/dist_tree/merge_ids.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import Pypdm.Pypdm        as PDM
+from maia import npy_pdm_gnum_dtype as pdm_dtype
+
+from maia.utils.parallel import utils as par_utils
+from maia.tree_exchange.dist_to_part import data_exchange as BTP
+from maia.tree_exchange.part_to_dist import data_exchange as PTB
+
+def _to_offset(array, last):
+  """Util fonction who transform a list of sorted indices into
+  and offset array """
+  if array.shape[0] == 0:
+    return np.array([0,last], dtype=array.dtype)
+  elif array[0] == 0 and array[-1] == last:
+    return array
+  elif array[0] != 0 and array[-1] != last:
+    offset = np.empty(array.shape[0]+2, dtype=array.dtype)
+    np.concatenate([[0], array, [last]], out=offset)
+    return offset
+  else:
+    offset = np.empty(array.shape[0]+1, dtype=array.dtype)
+    if array[0] != 0:
+      np.concatenate([[0], array], out=offset)
+    else:
+      np.concatenate([array, [last]], out=offset)
+    return offset
+
+def merge_distributed_ids(distri, ids, targets, comm):
+  """
+  Map some distributed elements (ids) to others (targets) and shift all the numbering,
+  in a distributed way.
+  ids and targets must be of same size and are distributed arrays
+  Return an old_to_new array for all the elements in the distribution
+  """
+
+  distri = distri.astype(pdm_dtype)
+  # Move data to procs holding ids, merging multiple elements
+  part_data = {'Targets' : [targets]}
+  pdm_distrib = par_utils.partial_to_full_distribution(distri, comm)
+
+  PTB = PDM.PartToBlock(comm, [ids.astype(pdm_dtype)], pWeight=None, partN=1,
+                        t_distrib=0, t_post=1, t_stride=0, userDistribution=pdm_distrib)
+  dist_ids  = PTB.getBlockGnumCopy()
+
+  dist_data = dict()
+  PTB.PartToBlock_Exchange(dist_data, part_data)
+
+  # Count the number of elements to be deleted (that is the number of elts received, after merge)
+  n_rmvd_local  = len(dist_ids)
+  n_rmvd_offset = par_utils.gather_and_shift(n_rmvd_local, comm)
+
+  #Initial old_to_new
+  old_to_new = np.arange(distri[0], distri[1]) + 1
+
+  # Shift local : for each index to remove, substract one to all the indices after him
+  # Nb : elements are sorted after part_to_dist
+  ids_local = dist_ids - distri[0] - 1
+  offset = _to_offset(ids_local, old_to_new.shape[0])
+  old_to_new -= np.repeat(np.arange(offset.shape[0]-1), np.diff(offset))
+
+  # Shift global : for each index, substract the number of targets removed by preceding ranks
+  old_to_new -= n_rmvd_offset[comm.Get_rank()]
+
+  # Now we need to update old_to_new for ids to indicate new indices of targets.
+  # Since the new index of target can be on another proc, we do a (fake) BTP to
+  # get the data using target numbering
+  dist_data2 = {'OldToNew' : old_to_new}
+  part_data2 = BTP.dist_to_part(distri, dist_data2, [dist_data['Targets'].astype(pdm_dtype)], comm)
+
+  old_to_new[ids_local] = part_data2['OldToNew'][0]
+
+  return old_to_new
---
+++ maia/transform/dist_tree/test/test_merge_ids.py
@@ -0,0 +1,45 @@
+import pytest
+from pytest_mpi_check._decorator import mark_mpi_test
+
+import numpy as np
+
+from maia.transform.dist_tree import merge_ids
+
+def test__to_offset():
+  assert (merge_ids._to_offset(np.array([0,2,7,13]), 13) == [0,2,7,13]).all()
+  assert (merge_ids._to_offset(np.array([0,2,7   ]), 13) == [0,2,7,13]).all()
+  assert (merge_ids._to_offset(np.array([2,7,13  ]), 13) == [0,2,7,13]).all()
+  assert (merge_ids._to_offset(np.array([2,7     ]), 13) == [0,2,7,13]).all()
+  assert (merge_ids._to_offset(np.array([        ]), 13) == [0,13]).all()
+
+@mark_mpi_test(2)
+def test_merge_distributed_ids(sub_comm):
+  if sub_comm.Get_rank() == 0:
+    distri = np.array([0,7,13], np.int32)
+    ids     = np.array([6,12,9], np.int32)
+    targets = np.array([1,10,7], np.int32)
+    expected_old_to_new = [1,2,3,4,5,1,6]
+  elif sub_comm.Get_rank() == 1:
+    distri = np.array([7,13,13], dtype=np.int32)
+    ids     = np.empty(0, dtype=np.int32)
+    targets = np.empty(0, dtype=np.int32)
+    expected_old_to_new = [7,6,8,9,8,10]
+
+  print("\n")
+  old_to_new = merge_ids.merge_distributed_ids(distri, ids, targets, sub_comm)
+  assert (old_to_new == expected_old_to_new).all()
+
+  if sub_comm.Get_rank() == 0:
+    distri = np.array([0,7,13])
+    ids     = np.array([6,9])
+    targets = np.array([1,7])
+    expected_old_to_new = [1,2,3,4,5,1,6]
+  elif sub_comm.Get_rank() == 1:
+    distri = np.array([7,13,13])
+    ids     = np.array([12])
+    targets = np.array([10])
+    expected_old_to_new = [7,6,8,9,8,10]
+
+  old_to_new = merge_ids.merge_distributed_ids(distri, ids, targets, sub_comm)
+  assert (old_to_new == expected_old_to_new).all()
+
