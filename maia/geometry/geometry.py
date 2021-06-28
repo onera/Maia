5bf6896ba6c253524f3f1bebd0733a1e61ec50c8
Sebastien Bourasseau <sebastien.bourasseau@onera.fr>
2021-06-28 18:03:10 +0200

    Apply transformation

--- maia/geometry/geometry.py
+++ maia/geometry/geometry.py
@@ -1,6 +1,8 @@
 from cmaia.geometry.geometry import *
 
 import numpy as np
+from math import cos,sin
+
 import Converter.Internal       as I
 import maia.sids.Internal_ext   as IE
 import maia.sids.sids           as sids
@@ -25,3 +27,19 @@ def compute_cell_center(zone):
     center_cell = compute_center_cell_s(*sids.Zone.CellSize(zone), cx, cy, cz)
 
   return center_cell
+
+  def applyTransformation(rotation_center, rotation_angle, translation, cx, cy, cz):
+  alpha, beta, gamma  = rotation_angle
+  rotation_matx = np.matrix([[1, 0, 0], [0, cos(alpha), -sin(alpha)], [0, sin(alpha), cos(alpha)]])
+  rotation_maty = np.matrix([[cos(beta), 0, sin(beta)], [0, 1, 0], [-sin(beta), 0, cos(beta)]])
+  rotation_matz = np.matrix([[cos(gamma), -sin(gamma), 0], [sin(gamma), cos(gamma), 0], [0, 0, 1]])
+  rotation_mat  = np.dot(rotation_matx, np.dot(rotation_maty, rotation_matz))
+  coords = np.array([cx,cy,cz], order='F')
+  modifiedCoords = np.dot(rotation_mat, np.array([cx,cy,cz], order='F')).T + translation
+  modCx = np.empty_like(cx)
+  modCy = np.empty_like(cy)
+  modCz = np.empty_like(cz)
+  modCx[:] = modifiedCoords[:,0].reshape(-1)
+  modCy[:] = modifiedCoords[:,1].reshape(-1)
+  modCz[:] = modifiedCoords[:,2].reshape(-1)
+  return (modCx,modCy,modCz)
