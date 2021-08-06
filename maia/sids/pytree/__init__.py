67b1787418ff9dfb9ba91c07e30b34ff563015ef
Julien Coulet <julien.coulet@onera.fr>
2021-08-06 14:33:23 +0200

    Move family generation and unplug family utils

--- maia/sids/pytree/__init__.py
+++ maia/sids/pytree/__init__.py
@@ -1,6 +1,5 @@
 from .compare import *
 from .generate import *
-from .family_utils import *
 from .remove_nodes import *
 from .node_walker import *
 from .nodes_walker import *
--- maia/sids/pytree/family_utils.py
+++ maia/sids/pytree/family_utils.py
@@ -85,6 +85,69 @@ def iterAllGridConnectivitiesFromAdditionalFamily(parent, family_name):
       yield node
   return nodes
 
+
+#Generation for families
+
+def create_get_from_family(label, family_label):
+  def _get_from_family(parent, family_name):
+    if isinstance(family_name, str):
+      family_name = [family_name]
+    for node in getNodesFromLabel(parent, label):
+      family_name_node = requestNodeFromLabel(node, family_label)
+      if family_name_node and I.getValue(family_name_node) in family_name:
+        return node
+    raise ValueError(f"Unable to find {label} from family name : {family_name}")
+  return _get_from_family
+
+
+def create_get_all_from_family(label, family_label):
+  def _get_all_from_family(parent, family_name):
+    if isinstance(family_name, str):
+      family_name = [family_name]
+    nodes = []
+    for node in getNodesFromLabel(parent, label):
+      family_name_node = requestNodeFromLabel(node, family_label)
+      if family_name_node and I.getValue(family_name_node) in family_name:
+        nodes.append(node)
+    return nodes
+  return _get_all_from_family
+
+def create_iter_all_from_family(label, family_label):
+  def _iter_all_from_family(parent, family_name):
+    if isinstance(family_name, str):
+      family_name = [family_name]
+    nodes = []
+    for node in getNodesFromLabel(parent, label):
+      family_name_node = requestNodeFromLabel(node, family_label)
+      if family_name_node and I.getValue(family_name_node) in family_name:
+        yield node
+  return _iter_all_from_family
+
+for family_label in ['Family_t', 'AdditionalFamily_t']:
+  for label in ['Zone_t', 'BC_t', 'ZoneSubRegion_t', 'GridConnectivity_t', 'GridConnectivity1to1_t', 'OversetHoles_t']:
+    name = "ToUpdate"  #TODO : update name
+    funcname = f"get{label[:-2]}From{family_label[:-2]}"
+    func = create_get_from_family(label, family_label)
+    func.__name__ = funcname
+    func.__doc__  = """Return a CGNS node from {0} with name {1}.""".format(family_label, name)
+    setattr(module_object, funcname, func)
+    setattr(module_object, PYU.camel_to_snake(funcname), func)
+
+    funcname = f"getAll{label[:-2]}From{family_label[:-2]}"
+    func = create_get_all_from_family(label, family_label)
+    func.__name__ = funcname
+    func.__doc__  = """Return a list of all CGNS node from {0} with name {1}.""".format(family_label, name)
+    setattr(module_object, funcname, func)
+    setattr(module_object, PYU.camel_to_snake(funcname), func)
+
+    funcname = f"getAll{label[:-2]}From{family_label[:-2]}"
+    func = create_iter_all_from_family(label, family_label)
+    func.__name__ = funcname
+    func.__doc__  = """Iterates on CGNS node from {0} with name {1}.""".format(family_label, name)
+    setattr(module_object, funcname, func)
+    setattr(module_object, PYU.camel_to_snake(funcname), func)
+
+
 get_family                  = getFamily
 get_grid_connectivities_from_family      = getGridConnectivitiesFromFamily
 get_all_grid_connectivities_from_family  = getAllGridConnectivitiesFromFamily
