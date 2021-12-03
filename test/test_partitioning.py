f2fcead3b589e309de1d7c20618a4bfdabe97232
Julien Coulet <julien.coulet@onera.fr>
2021-12-03 18:38:25 +0100

    Cleanup partitioning tests

--- test/test_partitioning.py
+++ test/test_partitioning.py
@@ -1,117 +1,136 @@
+import pytest
+from   pytest_mpi_check._decorator import mark_mpi_test
+import os
 from mpi4py import MPI
-import logging as LOG
 
-# ------------------------------------------------------------------------
-# > Initilise MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-
-# ---------------------------------------------------------
-fmt = '%(levelname)s:%(message)s '.format(rank, size)
-LOG.basicConfig(filename = '{0}.{1}.log'.format('maia_workflow_log', rank),
-                level    = 10,
-                format   = fmt,
-                filemode = 'w')
-# ---------------------------------------------------------
-
-import Converter.PyTree   as C
 import Converter.Internal as I
-import numpy              as NPY
-import sys
 
-from maia.cgns_io            import load_collective_size_tree       as LST
-from maia.cgns_io            import cgns_io_tree                    as IOT
-from maia.cgns_io            import save_part_tree                  as SPT
-from maia.cgns_io.hdf_filter import tree                            as HTF
-from maia.connectivity       import generate_ngon_from_std_elements as FTH
-from maia.partitioning       import part                            as PPA
-from maia.partitioning.load_balancing import setup_partition_weights as DBA
-import maia.distribution.distribution_tree                          as MDI
+import maia
+from   maia.utils            import test_utils as TU
 
-from   Converter import cgnskeywords as CGK
+from maia.cgns_io            import cgns_io_tree                     as IOT
+from maia.cgns_io            import save_part_tree                   as SPT
+from maia.partitioning       import part                             as PPA
+from maia.partitioning.load_balancing import setup_partition_weights as SPW
 
 
-# ------------------------------------------------------------------------
-# > Pick a file
-inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseU_C1_Cube.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseS_C1_Cube.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseS_C1_Cube_OnePerio.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseS_C1_Cube.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseU_C1_Cube_NGON2.hdf'
-inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseU_C1_Cube_BND_NGON2.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseU_C1_Cube_NGON2_FS.hdf'
-# inputfile    = '/home/bmaugars/dev/dev-Tools/etc/test/pypart/data/CaseU_C1_Cube_NGON2_FS_And_ZSR.hdf'
+@mark_mpi_test([3])
+def test_load_balancing(sub_comm):
+  """
+  An important input of partioning method is the dictionnary
+  zone_to_parts. This dictionnary allow to control the number of partitions
+  produced on each rank, as well as their size (if parmetis is used)
 
-# import Pypdm.Pypdm        as PDM
-# from Pypdm import Pypdm   as PDM
-# print(dir(PDM))
-# t1 = PDM.T1(10)
-# print(type(t1))
-# PDM.une_function(t1)
+  The dictionnary map the name of the initial block to a list : the size
+  of the list is the number of partitions to create for this rank, and
+  the value are the wanted weight (in % of cell) of each partition. Empty
+  list means this rank will hold no partition for this block.
+  The sum of weights for a given block should be 1.
 
-# ------------------------------------------------------------------------
-# > Load only the list of zone and sizes ...
-dist_tree = LST.load_collective_size_tree(inputfile, comm)
+  In this example, we have 3 procs and two blocks
+  """
 
-# I.printTree(dist_tree)
-# exit(2)
-# > ParaDiGM : dcube_gen() --> A faire
+  # > This mesh has two blocks of different sizes (768 & 192 cells)
+  mesh_file = os.path.join(TU.mesh_dir, 'S_twoblocks.yaml')
+  dist_tree = IOT.file_to_dist_tree(mesh_file, sub_comm)
 
-MDI.add_distribution_info(dist_tree, comm, distribution_policy='uniform')
-# I.printTree(dist_tree)
+  """ zone_to_parts dict can be specified by hand eg in this way : """
+  zone_to_parts = {}
+  if sub_comm.Get_rank() == 0:
+    zone_to_parts['Small'] = []
+    zone_to_parts['Large'] = [.25]
+  if sub_comm.Get_rank() == 1:
+    zone_to_parts['Small'] = [1.] #Small block will not be cutted
+    zone_to_parts['Large'] = [.25]
+  if sub_comm.Get_rank() == 2:
+    zone_to_parts['Small'] = []
+    zone_to_parts['Large'] = [.1, .1, .2, .1] #Large block will be cutted in several parts !
+  assert sub_comm.allreduce(sum(zone_to_parts['Small']), MPI.SUM) == 1. and \
+         sub_comm.allreduce(sum(zone_to_parts['Large']), MPI.SUM) == 1.
 
-hdf_filter = dict()
-HTF.create_tree_hdf_filter(dist_tree, hdf_filter)
+  """ Maia provide some helpers to automatically compute zone_to_parts dict :
 
-# for key, val in hdf_filter.items():
-#   print(key, val)
+  n_part_per_zone function make each rank to request n (usually one) partition
+  on each block. Weights will thus be homogeous : """
+  zone_to_parts = SPW.npart_per_zone(dist_tree, sub_comm, 1)
 
-# skip_type_ancestors = ["Zone_t/FlowSolution_t/"]
-# skip_type_ancestors = [[CGK.Zone_t, "FlowSolution#EndOfRun"], ["ZoneSubRegion_t", "VelocityY"]]
-# skip_type_ancestors = [[CGK.Zone_t, "FlowSolution#EndOfRun", "*"], ["Zone_t", "ZoneSubRegion_t", "VelocityY"]]
-skip_type_ancestors = [[CGK.Zone_t, "FlowSolution#EndOfRun", "Momentum*"],
-                       ["Zone_t", "ZoneSubRegion_t", "Velocity*"]]
-# hdf_filter_wo_fs = HTF.filtering_filter(dist_tree, hdf_filter, skip_type_ancestors, skip=True)
-# # IOT.load_tree_from_filter(inputfile, dist_tree, comm, hdf_filter)
+  for zone in I.getZones(dist_tree):
+    assert len(zone_to_parts[I.getName(zone)]) == 1
+    assert zone_to_parts[I.getName(zone)][0] == 1. / sub_comm.Get_size()
 
-# for key, val in hdf_filter_wo_fs.items():
-#   print(key, val)
-# IOT.load_tree_from_filter(inputfile, dist_tree, comm, hdf_filter_wo_fs)
-IOT.load_tree_from_filter(inputfile, dist_tree, comm, hdf_filter)
+  """ Note that n_part can be different for each proc :"""
+  n_part = 3 if sub_comm.Get_rank == 1 else 1
+  zone_to_parts = SPW.npart_per_zone(dist_tree, sub_comm, n_part)
 
-# FTH.generate_ngon_from_std_elements(dist_tree, comm)
+  for zone in I.getZones(dist_tree):
+    assert len(zone_to_parts[I.getName(zone)]) == n_part
+    assert zone_to_parts[I.getName(zone)][0] == 1. / sub_comm.allreduce(n_part, MPI.SUM)
 
-# C.convertPyTree2File(dist_tree, "dist_tree_{0}.hdf".format(rank))
+  """ the other function balance_multizone_tree try to equilibrate the total number of
+  partitioned cells for each rank while minimizing the number of partitions : """
 
-# I.printTree(dist_tree)
-# > To copy paste in new algorithm
-# dzone_to_proc = compute_distribution_of_zones(dist_tree, distribution_policy='uniform', comm)
-# > dzone_to_weighted_parts --> Proportion de la zone initiale qu'on souhate après partitionnement
-# > dloading_procs        --> Proportion de la zone initiale avant le partitionnement (vision block)
-#
-# > ... and this is suffisent to predict your partitions sizes
+  zone_to_parts = SPW.balance_multizone_tree(dist_tree, sub_comm)
 
-dzone_to_weighted_parts = DBA.npart_per_zone(dist_tree, comm, 1)
+  """ We can see that small zone will not be cutted """
+  assert sub_comm.allreduce(len(zone_to_parts['Small']), MPI.SUM) == 1 and \
+         sub_comm.allreduce(len(zone_to_parts['Large']), MPI.SUM) == 3
+  assert sub_comm.allreduce(sum(zone_to_parts['Small']), MPI.SUM) == 1. and \
+         sub_comm.allreduce(sum(zone_to_parts['Large']), MPI.SUM) == 1.
 
-# print(dzone_to_weighted_parts)
+@mark_mpi_test([3])
+def test_part_S(sub_comm, write_output):
+  mesh_file = os.path.join(TU.mesh_dir, 'S_twoblocks.yaml')
+  dist_tree = IOT.file_to_dist_tree(mesh_file, sub_comm)
 
-dloading_procs = dict()
-for zone in I.getZones(dist_tree):
-  dloading_procs[zone[0]] = list(range(comm.Get_size()))
-# print(dloading_procs)
+  zone_to_parts = SPW.npart_per_zone(dist_tree, sub_comm, 2)
+  part_tree = PPA.partitioning(dist_tree, sub_comm, zone_to_parts=zone_to_parts)
+  assert len(I.getZones(part_tree)) == 2*2
 
-part_tree = PPA.partitioning(dist_tree, comm, zone_to_parts=dzone_to_weighted_parts)
+  if write_output:
+    out_dir = TU.create_pytest_output_dir(sub_comm)
+    SPT.save_part_tree(part_tree, os.path.join(out_dir, 'part_tree'), sub_comm)
 
-for zone in I.getZones(part_tree):
-  fs_n = I.newFlowSolution(name="FlowSolution#EndOfRun", gridLocation='Vertex', parent=zone)
-  vtx_gi_n = I.getNodeFromName(zone, "np_vtx_ghost_information")
-  I.newDataArray("GhostInfo", vtx_gi_n[1], parent=fs_n)
+@pytest.mark.parametrize("graph_part_tool", ["parmetis", "ptscotch"])
+@mark_mpi_test([2])
+def test_part_elements(sub_comm, graph_part_tool, write_output):
+
+  mesh_file = os.path.join(TU.mesh_dir, 'Uelt_M6Wing.yaml')
+  dist_tree = IOT.file_to_dist_tree(mesh_file, sub_comm)
+
+  #Note : zone_to_parts defaults to 1part_per_zone
+  part_tree = PPA.partitioning(dist_tree, sub_comm, graph_part_tool=graph_part_tool)
+  assert len(I.getZones(part_tree)) == 1
+
+  if write_output:
+    out_dir = TU.create_pytest_output_dir(sub_comm)
+    SPT.save_part_tree(part_tree, os.path.join(out_dir, 'part_tree'), sub_comm)
 
 
-# I.printTree(part_tree)
-SPT.save_part_tree(part_tree, 'part_tree', comm)
-# C.convertPyTree2File(part_tree, "part_tree_{0}.hdf".format(rank))
+renum_methods_to_test = ["NONE", "HILBERT"]
+if maia.pdma_enabled:
+  renum_methods_to_test.append("CACHEBLOCKING2")
+@pytest.mark.parametrize("cell_renum_method", renum_methods_to_test)
+@mark_mpi_test([2])
+def test_part_NGon(sub_comm, cell_renum_method, write_output):
 
-# size_tree         = LST.load_collective_size_tree(inputfile, comm, ['CGNSBase_t/Zone_t',
-#                                                                        'CGNSBase_t/Family_t'/*])
+  #Todo : replace with naca3 zones
+  mesh_file = os.path.join(TU.mesh_dir, 'U_ATB_45.yaml')
+  dist_tree = IOT.file_to_dist_tree(mesh_file, sub_comm)
+
+  zone_to_parts = SPW.balance_multizone_tree(dist_tree, sub_comm)
+
+  #Different reordering methods can be applied after partitioning
+  reordering = {'cell_renum_method' : cell_renum_method,
+                'face_renum_method' : 'NONE',
+                'n_cell_per_cache'  : 0,
+                'n_face_per_pack'   : 0,
+                'graph_part_tool'   : 'parmetis' }
+
+  part_tree = PPA.partitioning(dist_tree, sub_comm, zone_to_parts=zone_to_parts, reordering=reordering)
+
+  assert len(I.getZones(part_tree)) == sum([len(zone_to_parts[zone]) for zone in zone_to_parts])
+
+  if write_output:
+    out_dir = TU.create_pytest_output_dir(sub_comm)
+    SPT.save_part_tree(part_tree, os.path.join(out_dir, 'part_tree'), sub_comm)
+
