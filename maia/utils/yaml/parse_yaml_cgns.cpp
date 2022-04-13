ae34dd80c6b3a334fd31c1f591ea275f92e06b3d
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2022-04-13 13:43:11 +0200

    Corrected compilation error with GCC 11

--- maia/utils/yaml/parse_yaml_cgns.cpp
+++ maia/utils/yaml/parse_yaml_cgns.cpp
@@ -23,9 +23,9 @@ auto
 to_nodes(const std::string& yaml_str) -> std::vector<cgns::tree> {
   std_e::throw_if_no_python_interpreter(__func__);
   auto parse_yaml_cgns = py::module_::import("maia.utils.parse_yaml_cgns");
-  py::list py_trees = parse_yaml_cgns.attr("to_nodes")(yaml_str);
 
   #if defined REAL_GCC && __GNUC__ >= 11
+    auto py_trees = parse_yaml_cgns.attr("to_nodes")(yaml_str);
     auto to_cpp_tree_copy_fn = [](const py::handle& py_tree) {
       return
         cgns::to_cpp_tree_copy(
@@ -34,6 +34,7 @@ to_nodes(const std::string& yaml_str) -> std::vector<cgns::tree> {
     };
     return py_trees | std::views::transform(to_cpp_tree_copy_fn) | std_e::to_vector();
   #else // Note : with the previous code, GCC 10 would call the cgns::tree copy ctor: this is not what we want
+    py::list py_trees = parse_yaml_cgns.attr("to_nodes")(yaml_str);
     int n_node = len(py_trees);
     std::vector<cgns::tree> res(n_node);
     for (int i=0; i<n_node; ++i) {
