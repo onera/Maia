3d7ca444e4ce1695f4ce8991b552ccf755bb119b
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2022-08-23 13:18:28 +0200

    adding 'poly' aliases to 'ngon' functions

--- maia/algo/dist/__init__.py
+++ maia/algo/dist/__init__.py
@@ -9,6 +9,7 @@ from .duplicate                  import duplicate_zone_with_transformation,\
                                         duplicate_from_rotation_jns_to_360
 
 from .elements_to_ngons          import elements_to_ngons
+elements_to_poly = elements_to_ngons
 
 from .extract_surf_dmesh         import extract_surf_tree_from_bc
 
@@ -18,10 +19,12 @@ from .merge                      import merge_connected_zones,\
 from .ngon_from_std_elements     import generate_ngon_from_std_elements
 
 from .ngons_to_elements          import ngons_to_elements
+poly_to_elements = ngons_to_elements
 
 from .rearrange_element_sections import rearrange_element_sections
 
 from .s_to_u                     import convert_s_to_u, convert_s_to_ngon
+convert_s_to_poly = convert_s_to_ngon
 
 from .vertex_list                import generate_jn_vertex_list,\
                                         generate_jns_vertex_list
