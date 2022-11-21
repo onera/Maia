eb14bcb572efc53ec28c416c171e397c98509e09
Clément Benazet <clement.benazet@onera.fr>
2022-11-21 11:49:11 +0100

    IO/__init__.py modif for doc tests

--- maia/io/__init__.py
+++ maia/io/__init__.py
@@ -1,5 +1,6 @@
 from .cgns_io_tree import file_to_dist_tree, \
                           dist_tree_to_file, \
-                          read_tree,        \
-                          write_tree,        \
-                          write_trees
+                          read_tree, \
+                          write_tree, write_trees
+
+from .save_part_tree import save_part_tree as part_tree_to_file
