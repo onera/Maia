e13b1f86211b32f6baa5beab33aecab291356a4a
Julien Coulet <julien.coulet@onera.fr>
2022-12-21 10:06:18 +0100

    Make functions operate inplace + cosmetics

--- maia/algo/dist/redistribute.py
+++ maia/algo/dist/redistribute.py
@@ -16,13 +16,10 @@ def redistribute_pl_node(node, distribution, comm):
   using a given distribution function. Mainly useful for unit tests. Node must be known by
   each process.
   """
-  node_distrib = PT.get_node_from_path(node, ':CGNS#Distribution/Index')[1]
-  n_elt        = node_distrib[2]
-  PT.rm_node_from_path(node,  ":CGNS#Distribution")
-
-  # New distribution
-  new_distrib = distribution(n_elt, comm)
+  node_distrib = MT.getDistribution(node, 'Index')[1]
+  new_distrib = distribution(node_distrib[2], comm)
   MT.newDistribution({'Index' : new_distrib}, node)
+
   #PL and PLDonor
   for array_n in PT.get_children_from_predicate(node, 'IndexArray_t'):
     array_n[1] = MTP.block_to_block(array_n[1][0], node_distrib, new_distrib, comm).reshape(1,-1, order='F')
@@ -38,11 +35,8 @@ def redistribute_pl_node(node, distribution, comm):
   #Additionnal treatement for subnodes with PL (eg bcdataset)
   has_pl = lambda n : PT.get_name(n) not in ['PointList', 'PointRange'] and has_subset(n)
   for child in [node for node in PT.get_children(node) if has_pl(node)]:
-    dist_child = redistribute_pl_node(child, comm)
-    child[2] = dist_child[2]
+    redistribute_pl_node(child, comm)
 
-
-  return node
 # ---------------------------------------------------------------------------------------
 
 
@@ -57,7 +51,6 @@ def redistribute_data_node(node, distri, new_distri, comm):
   for array in PT.iter_children_from_label(node, 'DataArray_t'):
     array[1] = MTP.block_to_block(array[1], distri, new_distri, comm)
 
-  return node
 # ---------------------------------------------------------------------------------------
 
 
@@ -134,36 +127,30 @@ def redistribute_elements_node(node, distribution, comm):
       new_pe[:,ip] = new_pe_tmp
     PT.set_value(pe_n, new_pe)
 
-  return node # Pas top si y'a pas de deep copy ?
 # ---------------------------------------------------------------------------------------
 
 
 # ---------------------------------------------------------------------------------------
-def redistribute_zone(dist_zone, distribution, comm):
-  zone = dist_zone # OR deep_copy ?? 
+def redistribute_zone(zone, distribution, comm):
 
   # Get distribution
-  vtx_distrib  = PT.get_node_from_path(zone, ":CGNS#Distribution/Vertex")[1]
-  cell_distrib = PT.get_node_from_path(zone, ":CGNS#Distribution/Cell")[1]
-  PT.rm_node_from_path(zone,  ":CGNS#Distribution")
+  vtx_distrib  = MT.getDistribution(zone, "Vertex")[1]
+  cell_distrib = MT.getDistribution(zone, "Cell")[1]
+  old_distrib = {'Vertex' : vtx_distrib ,
+                 'Cell'   : cell_distrib}
 
   # New distribution
-  new_vtx_distrib  = distribution(vtx_distrib[2] , comm)
-  new_cell_distrib = distribution(cell_distrib[2], comm)
+  new_vtx_distrib  = distribution(PT.Zone.n_vtx(zone) , comm)
+  new_cell_distrib = distribution(PT.Zone.n_cell(zone), comm)
   new_distrib = {'Vertex' : new_vtx_distrib ,
                  'Cell'   : new_cell_distrib}
   MT.newDistribution(new_distrib, zone)
-  
+   
 
   # > Coords
   grid_coords = PT.get_children_from_label(zone, 'GridCoordinates_t')
   for grid_coord in grid_coords:
-    for direction in ["CoordinateX", "CoordinateY", "CoordinateZ"]:
-      coord_n = PT.get_node_from_name(grid_coord, direction)
-      coord   = PT.get_node_from_name(grid_coord, direction)[1]
-      new_coord = MTP.block_to_block(coord, vtx_distrib, new_vtx_distrib, comm)
-      PT.set_value(coord_n, new_coord)
-
+    redistribute_data_node(grid_coord, vtx_distrib, new_vtx_distrib, comm)
 
   # > Elements
   elts = PT.get_children_from_label(zone, 'Elements_t')
@@ -174,36 +161,20 @@ def redistribute_zone(dist_zone, distribution, comm):
   # > Flow Solutions
   sols = PT.get_children_from_label(zone, 'FlowSolution_t') + PT.get_children_from_label(zone, 'DiscreteData_t')
   for sol in sols:
-    PT.rm_child(zone, sol)
-
-    grid_location = PT.get_value(PT.get_child_from_label(sol, 'GridLocation_t'))
-    if   grid_location == 'Vertex'      : distrib = vtx_distrib  ; new_distrib = new_vtx_distrib
-    elif grid_location == 'CellCenter'  : distrib = cell_distrib ; new_distrib = new_cell_distrib
-    else                                : raise Exception('Redistribute : Only Vertex and CellCenter FlowSolution are supported yet.')
-    
     if PT.get_child_from_name(sol, 'PointList') is None:
-      PT.add_child(zone, redistribute_data_node(sol, distrib, new_distrib, comm))
+      loc = PT.Subset.GridLocation(sol)
+      redistribute_data_node(sol, old_distrib[loc], new_distrib[loc], comm)
     else:
-      PT.add_child(zone, redistribute_pl_node(sol, distribution, comm))
-
+      redistribute_pl_node(sol, distribution, comm)
 
   # > BCs
-  zonebcs = PT.get_children_from_label(zone, 'ZoneBC_t')
-  for zonebc in zonebcs:
-    PT.rm_child(zone, zonebc)
-    dist_zonebc = PT.new_child(zone, PT.get_name(zonebc), 'ZoneBC_t')
-    for bc in PT.iter_children_from_label(zonebc, 'BC_t'):
-      PT.add_child(dist_zonebc, redistribute_pl_node(bc, distribution, comm))
-
+  for bc in PT.iter_children_from_predicates(zone, 'ZoneBC_t/BC_t'):
+    redistribute_pl_node(bc, distribution, comm)
 
   # > GCs
-  zonegcs = PT.get_children_from_label(zone, 'ZoneGridConnectivity_t')
-  for zonegc in zonegcs:
-    PT.rm_child(zone, zonegc)
-    dist_zonegc = PT.new_child(zone, PT.get_name(zonegc), 'ZoneGridConnectivity_t')
-    for gc in PT.get_children_from_label(zonegc, 'GridConnectivity_t') + PT.get_children_from_label(zonegc, 'GridConnectivity1to1_t'):
-      PT.add_child(dist_zonegc, redistribute_pl_node(gc, distribution, comm))
-
+  gc_pred = lambda n: PT.get_label(n) in ['GridConnectivity_t', 'GridConnectivity1to1_t']
+  for gc in PT.iter_children_from_predicates(zone, ['ZoneGridConnectivity_t', gc_pred]):
+    redistribute_pl_node(gc, distribution, comm)
 
   # > ZoneSubRegion
   zone_subregions = PT.get_children_from_label(zone, 'ZoneSubRegion_t')
@@ -213,7 +184,7 @@ def redistribute_zone(dist_zone, distribution, comm):
     if matching_region_path != PT.get_name(zone_subregion):
       PT.add_child(zone_subregion, PT.get_node_from_path(zone, matching_region_path + '/PointList'))
       PT.add_child(zone_subregion, PT.get_node_from_path(zone, matching_region_path + '/PointRange'))
-    dist_zone_subregion = redistribute_pl_node(zone_subregion, distribution, comm)
+    redistribute_pl_node(zone_subregion, distribution, comm)
     if matching_region_path != PT.get_name(zone_subregion):
       PT.rm_children_from_name(dist_zone_subregion, 'PointList')
       PT.rm_children_from_name(dist_zone_subregion, 'PointRange')
@@ -222,7 +193,6 @@ def redistribute_zone(dist_zone, distribution, comm):
     PT.rm_child(zone, zone_subregion)
     PT.add_child(zone, dist_zone_subregion)
     
-  return zone
 # ---------------------------------------------------------------------------------------
 
 
