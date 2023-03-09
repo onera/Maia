9da0f190c4aba78c60e6063e29fa7e8e357fb7ea
Julien Coulet <julien.coulet@onera.fr>
2023-03-09 18:06:41 +0100

    Change default maia logger names

--- maia/utils/logging/__init__.py
+++ maia/utils/logging/__init__.py
@@ -1,1 +1,9 @@
 from cmaia.utils.logging import *
+def info(msg):
+  log("maia", msg)
+def stat(msg):
+  log("maia-stats", msg)
+def warning(msg):
+  log("maia-warnings", "Warning: "+msg)
+def error(msg):
+  log("maia-errors", "Error: "+msg)
--- maia/utils/logging/default_loggers.cpp
+++ maia/utils/logging/default_loggers.cpp
@@ -5,8 +5,10 @@ namespace {
 
 bool init_maia_default_loggers() {
   // ensures that these loggers are present for maia to run even if no logging configuration file was found
-  std_e::add_logger_if_absent(std_e::logger{"maia"             ,std_e::mpi_stdout_printer{}});
-  std_e::add_logger_if_absent(std_e::logger{"maia.rank_0"      ,std_e::mpi_rank_0_stdout_printer{}});
+  std_e::add_logger_if_absent(std_e::logger{"maia"             ,std_e::mpi_rank_0_stdout_printer{}});
+  std_e::add_logger_if_absent(std_e::logger{"maia-warnings"    ,std_e::mpi_rank_0_stdout_printer{}});
+  std_e::add_logger_if_absent(std_e::logger{"maia-errors"      ,std_e::mpi_rank_0_stdout_printer{}});
+  std_e::add_logger_if_absent(std_e::logger{"maia-stats"       ,                                {}});
   std_e::add_logger_if_absent(std_e::logger{"maia perf level 0",std_e::mpi_rank_0_stdout_printer{}});
   std_e::add_logger_if_absent(std_e::logger{"maia perf level 1",std_e::mpi_stdout_printer{}});
   std_e::add_logger_if_absent(std_e::logger{"maia perf level 2",std_e::mpi_stdout_printer{}});
