3323b6e496d4a73768b7966b16841bb3c81dd729
Julien Coulet <julien.coulet@onera.fr>
2023-05-24 14:50:52 +0200

    Remove old connect match + update func. test

--- maia/algo/part/__init__.py
+++ maia/algo/part/__init__.py
@@ -6,8 +6,6 @@ from .closest_points         import find_closest_points
 
 from .connectivity_transform import enforce_boundary_pe_left
 
-from .connect_match          import connect_match_from_family
-
 from .extract_boundary       import extract_faces_mesh,\
                                     extract_surf_from_bc
 
--- maia/algo/part/geometry/geometry.pybind.hpp
+++ maia/algo/part/geometry/geometry.pybind.hpp
@@ -2,24 +2,6 @@
 #include <pybind11/numpy.h>
 
 // --------------------------------------------------------------------
-pybind11::array_t<int, pybind11::array::f_style>
-adapt_match_information(pybind11::array_t<int, pybind11::array::f_style>& np_neighbor_idx,
-                        pybind11::array_t<int, pybind11::array::f_style>& np_neighbor_desc,
-                        pybind11::array_t<int, pybind11::array::f_style>& np_recv_entity_stri,
-                        pybind11::array_t<int, pybind11::array::f_style>& np_point_list,
-                        pybind11::array_t<int, pybind11::array::f_style>& np_point_list_donor);
-
-// --------------------------------------------------------------------
-std::tuple<pybind11::array_t<double, pybind11::array::f_style>,
-           pybind11::array_t<double, pybind11::array::f_style>>
-compute_face_center_and_characteristic_length(pybind11::array_t<int   , pybind11::array::f_style>& np_point_list,
-                                              pybind11::array_t<double, pybind11::array::f_style>& np_cx,
-                                              pybind11::array_t<double, pybind11::array::f_style>& np_cy,
-                                              pybind11::array_t<double, pybind11::array::f_style>& np_cz,
-                                              pybind11::array_t<int   , pybind11::array::f_style>& np_face_vtx,
-                                              pybind11::array_t<int   , pybind11::array::f_style>& np_face_vtx_idx);
-
-// --------------------------------------------------------------------
 pybind11::array_t<double, pybind11::array::f_style>
 compute_center_cell_u(int n_cell,
                       pybind11::array_t<double, pybind11::array::f_style>& np_cx,
