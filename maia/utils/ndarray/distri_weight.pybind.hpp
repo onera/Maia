08533a12d2e7df269b84ce4c351f739f742a7021
Julien Coulet <julien.coulet@onera.fr>
2023-09-01 10:30:22 +0200

    Wrapping of pdm_compute_weighted_distribution no more needed (reverts 05fd5e0a)

--- maia/utils/ndarray/distri_weight.pybind.hpp
+++
@@ -1,8 +0,0 @@
-/*
- * TMP wrapping of PDM_distrib_weight -- To remove when Cython method is avalaible
-*/
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-#include <pdm.h>
-
-auto compute_weighted_distribution(pybind11::list, pybind11::list weights, pybind11::handle) -> pybind11::array_t<PDM_g_num_t>;
