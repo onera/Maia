c82b8d549e42ff2ccd4128aeaf0bb1014ce6fd05
Clément Benazet <clement.benazet@onera.fr>
2023-12-13 17:09:52 +0100

    [extract_part_s] add FaceCenter test

--- maia/algo/part/test/test_extract_part_s.py
+++ maia/algo/part/test/test_extract_part_s.py
@@ -8,16 +8,17 @@ import maia.pytree as PT
 
 from maia.algo.part import extract_part as EP
 
-def sample_part_tree(comm):
-  dist_tree = maia.factory.generate_dist_block(10, 'Structured', comm)
+def sample_part_tree(comm, bc_loc):
+  dist_tree = maia.factory.dcube_generator.dcube_struct_generate(10, 1., [0.,0.,0.], comm, bc_location=bc_loc)
   part_opts = maia.factory.partitioning.compute_regular_weights(dist_tree, comm, n_part=4)
   part_tree = maia.factory.partition_dist_tree(dist_tree, comm, zone_to_parts=part_opts)
   return part_tree
 
 @pytest_parallel.mark.parallel([1,3])
+@pytest.mark.parametrize("bc_loc" , ['Vertex','FaceCenter'])
 @pytest.mark.parametrize("bc_name", ['Xmin','Xmax','Ymin','Ymax','Zmin','Zmax'])
-def test_extract_part_simple(bc_name, comm):
-  part_tree = sample_part_tree(comm)
+def test_extract_part_simple(bc_loc, bc_name, comm):
+  part_tree = sample_part_tree(comm, bc_loc)
 
   extract_part_tree = maia.algo.part.extract_part_s.extract_part_s_from_bc_name(part_tree, bc_name, comm)
   extract_dist_tree = maia.factory.recover_dist_tree(extract_part_tree, comm)
