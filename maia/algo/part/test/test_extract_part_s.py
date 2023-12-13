0af9727dd5be04823c7f78c18fff3e7f183f0409
Clément Benazet <clement.benazet@onera.fr>
2023-12-13 09:43:58 +0100

    [extract_part_s] add test

---
+++ maia/algo/part/test/test_extract_part_s.py
@@ -0,0 +1,26 @@
+from mpi4py import MPI
+import pytest
+import pytest_parallel
+import numpy as np
+
+import maia
+import maia.pytree as PT
+
+from maia.algo.part import extract_part as EP
+
+def sample_part_tree(comm):
+  dist_tree = maia.factory.generate_dist_block(10, 'Structured', comm)
+  part_opts = maia.factory.partitioning.compute_regular_weights(dist_tree, comm, n_part=4)
+  part_tree = maia.factory.partition_dist_tree(dist_tree, comm, zone_to_parts=part_opts)
+  return part_tree
+
+@pytest_parallel.mark.parallel([1,3])
+@pytest.mark.parametrize("bc_name", ['Xmin','Xmax','Ymin','Ymax','Zmin','Zmax'])
+def test_extract_part_simple(bc_name, comm):
+  part_tree = sample_part_tree(comm)
+
+  extract_part_tree = maia.algo.part.extract_part_s.extract_part_s_from_bc_name(part_tree, bc_name, comm)
+  extract_dist_tree = maia.factory.recover_dist_tree(extract_part_tree, comm)
+  extract_dist_zone = PT.get_all_Zone_t(extract_dist_tree)[0]
+  assert PT.Zone.n_vtx( extract_dist_zone)==100
+  assert PT.Zone.n_cell(extract_dist_zone)==81
