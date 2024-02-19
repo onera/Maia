2d5b04ef1e9e0f81038e8d7296fe9358f46e3eb4
Bruno Maugars <bruno.maugars@onera.fr>
2024-02-19 10:04:36 +0100

    Preserve Ordinal_t nodes in split & recover

--- maia/factory/dist_from_part.py
+++ maia/factory/dist_from_part.py
@@ -210,7 +210,7 @@ def _recover_BC(dist_zone, part_zones, comm):
   bc_predicate = ['ZoneBC_t', 'BC_t']
 
   discover_nodes_from_matching(dist_zone, part_zones, bc_predicate, comm,
-        child_list=['FamilyName_t', 'GridLocation_t'], get_value='all')
+        child_list=['FamilyName_t', 'GridLocation_t', 'Ordinal_t'], get_value='all')
 
   for bc_path in PT.predicates_to_paths(dist_zone, bc_predicate):
     if PT.Zone.Type(dist_zone) == 'Unstructured':
