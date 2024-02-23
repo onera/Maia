39437d11a8e0b9e19ac233f575315ad50adee78d
Hedi El Amami <hedi.el_amami@onera.fr>
2024-02-23 13:45:19 +0100

    Fix issue void procs

--- maia/factory/dist_from_part.py
+++ maia/factory/dist_from_part.py
@@ -329,14 +329,28 @@ def recover_dist_tree(part_tree, comm):
     PT.set_value(dist_zone, d_zone_dims)
 
     # > Create vertex distribution and exchange vertex coordinates
-    p_grid_co = PT.get_node_from_label(part_tree, 'GridCoordinates_t')
-    p_gc_transform = PT.get_node_from_name(p_grid_co, 'CoordinateTransform')
-    is_coords = lambda n: [PT.get_node_from_label(child, 'DataArray_t') for child in PT.get_children(n) if PT.get_name(child) != "CoordinateTransform"]
-    p_grid_co_names = [PT.get_name(gc_node) for gc_node in is_coords(p_grid_co)]
     d_grid_co = PT.new_GridCoordinates('GridCoordinates', parent=dist_zone)
+    p_grid_co_names = []
+    p_gc_transform = []
+    tag_names = -1
+    tag_transform = -1 
+    for part_zone in part_zones:
+      p_grid_co = PT.get_node_from_label(part_zone, 'GridCoordinates_t')
+      is_coords = lambda n: [PT.get_node_from_label(child, 'DataArray_t') for child in PT.get_children(n) if PT.get_name(child) != "CoordinateTransform"]
+      p_grid_co_names = [PT.get_name(gc_node) for gc_node in is_coords(p_grid_co)]
+      p_gc_transform_n = PT.get_node_from_name(p_grid_co, 'CoordinateTransform')
+      if p_grid_co_names: tag_names = comm.rank
+      if p_gc_transform_n: tag_transform = comm.rank
+  
+    tag_names = comm.allreduce(tag_names, MPI.MAX)
+    p_grid_co_names = comm.bcast(p_grid_co_names, tag_names)
+
+    tag_transform = comm.allreduce(tag_transform, MPI.MAX)
+    if tag_transform != -1: p_gc_transform = comm.bcast(p_gc_transform, tag_names)
+
     for coord in p_grid_co_names:
       PT.new_DataArray(coord, value=None, parent=d_grid_co)
-    PT.add_child(d_grid_co, p_gc_transform)
+    if tag_transform != -1: PT.add_child(d_grid_co, p_gc_transform)
     PTB.part_coords_to_dist_coords(dist_zone, part_zones, comm)
 
     # > Create elements
