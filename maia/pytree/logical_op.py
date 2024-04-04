7e3b1e058043170f15ad6c53ed9a195c82de8892
Sebastien Bourasseau <sebastien.bourasseau@onera.fr>
2024-04-04 14:09:01 +0200

    [Pytree] Logical op - diff

--- maia/pytree/logical_op.py
+++ maia/pytree/logical_op.py
@@ -1,4 +1,4 @@
-import copy
+import copy as cp
 
 import maia.pytree as PT
 
@@ -7,7 +7,7 @@ def _add_children_to_node_from_another(node1, node2, copy=False):
         child1 = PT.get_child_from_name(node1, PT.get_name(child2))
         if child1 is None:
             if copy:
-                PT.add_child(node1, copy.deepcopy(child2))
+                PT.add_child(node1, cp.deepcopy(child2))
             else:
                 PT.add_child(node1, child2)
         else:
@@ -20,13 +20,42 @@ def _rm_not_common_children(node1, node2):
         child2 = PT.get_child_from_name(node2, child1_name)
         if child2 is None:
             child1_to_del.append(child1_name)
-    print(child1_to_del)
     for child_name in child1_to_del:
         PT.rm_node_from_path(node1, child_name)
     for child1 in PT.get_children(node1):
         child2 = PT.get_child_from_name(node2, PT.get_name(child1))
         if child2 is not None:
             _rm_not_common_children(child1, child2)
+            
+def _have_common_children(node1, node2):
+    for child1 in PT.get_children(node1):
+        child2 = PT.get_child_from_name(node2, PT.get_name(child1))
+        if child2 is None:
+            return False
+        else:
+            if not _have_common_children(child1, child2):
+                return False
+    return True
+
+def _rm_common_children(node1, node2):
+    print("SB")
+    child1_to_del = []
+    for child1 in PT.get_children(node1):
+        child1_name = PT.get_name(child1)
+        child2 = PT.get_child_from_name(node2, child1_name)
+        if child2 is not None:
+            print(child2[0], _have_common_children(child1, child2))
+            if _have_common_children(child1, child2):
+                child1_to_del.append(child1_name)
+    print(child1_to_del)
+    for child_name in child1_to_del:
+        PT.rm_node_from_path(node1, child_name)
+    for child1 in PT.get_children(node1):
+        print(child1[0])
+        child2 = PT.get_child_from_name(node2, PT.get_name(child1))
+        if child2 is not None:
+            print("Not None")
+            _rm_common_children(child1, child2)
 
 def union(node1, node2, copy=False):
     """
@@ -70,7 +99,7 @@ def union(node1, node2, copy=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     if copy:
-        union_nodes = copy.deepcopy(node1)
+        union_nodes = cp.deepcopy(node1)
     else:
         union_nodes = node1
     _add_children_to_node_from_another(union_nodes, node2, False)
@@ -80,6 +109,7 @@ def intersection(node1, node2, copy=False):
     """
     Return a new node intersection of node1 and node2
     Remark: node1 and node2 must have the same CGNS label
+    Remark: if the intersection is empty, return only the CGNSTree_t node
 
     Args:
       node1 (CGNSNode): First CGNS node
@@ -112,9 +142,52 @@ def intersection(node1, node2, copy=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     if copy:
-        intersect_nodes = copy.deepcopy(node1)
+        intersect_nodes = cp.deepcopy(node1)
     else:
         intersect_nodes = node1
     _rm_not_common_children(intersect_nodes, node2)
     return intersect_nodes
-    
+
+def diff(node1, node2, copy=False):
+    """
+    Return a new node that correspond to node1 without node2's nodes
+    Remark: node1 and node2 must have the same CGNS label
+    Remark: if the intersection is empty, return only the CGNSTree_t node
+
+    Args:
+      node1 (CGNSNode): First CGNS node
+      node2 (CGNSNode): Second CGNS node
+    Returns:
+      CGNSNode: differences between nodes
+    Example:
+      >>> tree1 = PT.yaml.parse_yaml_cgns.to_cgns_tree('''
+      ... CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
+      ... Base CGNSBase_t:
+      ...   Zone1 Zone_t:
+      ...     ZoneGridConnectivity ZoneGridConnectivity_t:
+      ...       match GridConnectivity1to1_t "Zone3":
+      ...   Zone2 Zone_t:
+      ... ''')
+      >>> tree2 = PT.yaml.parse_yaml_cgns.to_cgns_tree('''
+      ... CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
+      ... Base CGNSBase_t:
+      ...   Zone2 Zone_t:
+      ...   Zone3 Zone_t:
+      ...     ZoneGridConnectivity ZoneGridConnectivity_t:
+      ...       match GridConnectivity1to1_t "Zone1":
+      ... ''')
+      >>> maia.pytree.logical_op.intersection(tree1, tree2)
+      CGNSTree CGNSTree_t
+      └───Base CGNSBase_t
+          └───Zone1 Zone_t
+              └───ZoneGridConnectivity ZoneGridConnectivity_t
+                  └───match GridConnectivity1to1_t "Zone3":
+    """
+    if PT.get_label(node1) != PT.get_label(node2):
+        raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
+    if copy:
+        diff_nodes = cp.deepcopy(node1)
+    else:
+        diff_nodes = node1
+    _rm_common_children(diff_nodes, node2)
+    return diff_nodes
--- maia/pytree/test/test_logical_op.py
+++ maia/pytree/test/test_logical_op.py
@@ -113,21 +113,24 @@ def test_union():
   tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
   tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
   
-  union_tree1 = PLO.union(tree1a, tree1b)
-  assert PT.is_same_tree(union_tree1, ref_union_tree)
+  union_tree1ab = PLO.union(tree1a, tree1b, copy=True)
+  assert PT.is_same_tree(union_tree1ab, ref_union_tree)
+  union_tree1ba = PLO.union(tree1b, tree1a)
+  assert PT.is_same_tree(union_tree1ba, ref_union_tree)
   
   tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
   tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
   
-  union_tree2 = PLO.union(tree2a, tree2b)
-  assert PT.is_same_tree(union_tree2, ref_union_tree)
+  union_tree2ab = PLO.union(tree2a, tree2b, copy=True)
+  assert PT.is_same_tree(union_tree2ab, ref_union_tree)
+  union_tree2ba = PLO.union(tree2b, tree2a)
+  assert PT.is_same_tree(union_tree2ba, ref_union_tree)
 
 def test_intersection():
   intersect_yt1 = """
   CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
   """
   intersect_yt2 = """
-  CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
   BaseA CGNSBase_t:
     Zone1 Zone_t:
       ZGC ZoneGridConnectivity_t:
@@ -145,13 +148,100 @@ def test_intersection():
   tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
   tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
   
-  intersect_tree1 = PLO.intersection(tree1a, tree1b)
-  assert PT.is_same_tree(intersect_tree1, ref_intersect_tree1)
+  intersect_tree1ab = PLO.intersection(tree1a, tree1b, copy=True)
+  assert PT.is_same_tree(intersect_tree1ab, ref_intersect_tree1)
+  intersect_tree1ba = PLO.intersection(tree1b, tree1a)
+  assert PT.is_same_tree(intersect_tree1ba, ref_intersect_tree1)
   
   tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
   tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
   
-  intersect_tree2 = PLO.intersection(tree2a, tree2b)
-  PT.print_tree(intersect_tree2)
-  PT.print_tree(ref_intersect_tree2)
-  assert PT.is_same_tree(intersect_tree2, ref_intersect_tree2)
+  intersect_tree2ab = PLO.intersection(tree2a, tree2b, copy=True)
+  assert PT.is_same_tree(intersect_tree2ab, ref_intersect_tree2)
+  intersect_tree2ba = PLO.intersection(tree2b, tree2a)
+  assert PT.is_same_tree(intersect_tree2ba, ref_intersect_tree2)
+
+
+def test_diff():
+  diff_yt1ab = """
+  BaseA CGNSBase_t:
+    Zone1 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "Zone3":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+    Zone2 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "Zone4":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+    Zone3 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match1 GridConnectivity_t "BaseA/Zone1":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+        match2 GridConnectivity_t "BaseB/Zone6":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+    Zone4 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "Zone2":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+  """
+  diff_yt1ba = """
+  BaseB CGNSBase_t:
+    Zone5 Zone_t:
+    Zone6 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "BaseA/Zone3":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+  """
+  diff_yt2ab = """
+  BaseA CGNSBase_t:
+    Zone3 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match2 GridConnectivity_t "BaseB/Zone6":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+  """
+  diff_yt2ba = """
+  BaseA CGNSBase_t:
+    Zone2 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "Zone4":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+    Zone3 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match1 GridConnectivity_t "BaseA/Zone1":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+    Zone4 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "Zone2":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+  BaseB CGNSBase_t:
+    Zone5 Zone_t:
+    Zone6 Zone_t:
+      ZGC ZoneGridConnectivity_t:
+        match GridConnectivity_t "BaseA/Zone3":
+          GridConnectivityType GridConnectivityType_t "Abutting1to1":
+  """
+  ref_diff_tree1ab = parse_yaml_cgns.to_cgns_tree(diff_yt1ab)
+  ref_diff_tree1ba = parse_yaml_cgns.to_cgns_tree(diff_yt1ba)
+  ref_diff_tree2ab = parse_yaml_cgns.to_cgns_tree(diff_yt2ab)
+  ref_diff_tree2ba = parse_yaml_cgns.to_cgns_tree(diff_yt2ba)
+  #Need to delete CGNSLibraryVersion_t because to_cgns_tree creates it
+  PT.rm_node_from_path(ref_diff_tree1ab, 'CGNSLibraryVersion')
+  PT.rm_node_from_path(ref_diff_tree1ba, 'CGNSLibraryVersion')
+  PT.rm_node_from_path(ref_diff_tree2ab, 'CGNSLibraryVersion')
+  PT.rm_node_from_path(ref_diff_tree2ba, 'CGNSLibraryVersion')
+  
+  tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
+  tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
+  
+  diff_tree1ab = PLO.diff(tree1a, tree1b, copy=True)
+  assert PT.is_same_tree(diff_tree1ab, ref_diff_tree1ab)
+  diff_tree1ba = PLO.diff(tree1b, tree1a)
+  assert PT.is_same_tree(diff_tree1ba, ref_diff_tree1ba)
+  
+  tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
+  tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
+  
+  diff_tree2ab = PLO.diff(tree2a, tree2b, copy=True)
+  assert PT.is_same_tree(diff_tree2ab, ref_diff_tree2ab)
+  diff_tree2ba = PLO.diff(tree2b, tree2a)
+  assert PT.is_same_tree(diff_tree2ba, ref_diff_tree2ba)
