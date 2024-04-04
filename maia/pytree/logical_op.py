3940d94d19ebdd78179e075c987e84d56d265ca4
Sebastien Bourasseau <sebastien.bourasseau@onera.fr>
2024-04-04 16:17:58 +0200

    [Pytree] Logical op - change copy to True and replace copy.deepcopy by maia's one

--- maia/pytree/logical_op.py
+++ maia/pytree/logical_op.py
@@ -1,17 +1,15 @@
-import copy as cp
-
 import maia.pytree as PT
 
-def _add_children_to_node_from_another(node1, node2, copy=False):
+def _add_children_to_node_from_another(node1, node2, copy=True):
     for child2 in PT.get_children(node2):
         child1 = PT.get_child_from_name(node1, PT.get_name(child2))
         if child1 is None:
             if copy:
-                PT.add_child(node1, cp.deepcopy(child2))
+                PT.add_child(node1, PT.deep_copy(child2))
             else:
                 PT.add_child(node1, child2)
         else:
-            _add_children_to_node_from_another(child1, child2, copy=False)
+            _add_children_to_node_from_another(child1, child2, copy)
 
 def _rm_not_common_children(node1, node2):
     child1_to_del = []
@@ -38,26 +36,21 @@ def _have_common_children(node1, node2):
     return True
 
 def _rm_common_children(node1, node2):
-    print("SB")
     child1_to_del = []
     for child1 in PT.get_children(node1):
         child1_name = PT.get_name(child1)
         child2 = PT.get_child_from_name(node2, child1_name)
         if child2 is not None:
-            print(child2[0], _have_common_children(child1, child2))
             if _have_common_children(child1, child2):
                 child1_to_del.append(child1_name)
-    print(child1_to_del)
     for child_name in child1_to_del:
         PT.rm_node_from_path(node1, child_name)
     for child1 in PT.get_children(node1):
-        print(child1[0])
         child2 = PT.get_child_from_name(node2, PT.get_name(child1))
         if child2 is not None:
-            print("Not None")
             _rm_common_children(child1, child2)
 
-def union(node1, node2, copy=False):
+def union(node1, node2, copy=True):
     """
     Return a new node union of node1 and node2
     Remark: node1 and node2 must have the same CGNS label
@@ -99,13 +92,13 @@ def union(node1, node2, copy=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     if copy:
-        union_nodes = cp.deepcopy(node1)
+        union_nodes = PT.deep_copy(node1)
     else:
         union_nodes = node1
-    _add_children_to_node_from_another(union_nodes, node2, False)
+    _add_children_to_node_from_another(union_nodes, node2, copy)
     return union_nodes
 
-def intersection(node1, node2, copy=False):
+def intersection(node1, node2, copy=True):
     """
     Return a new node intersection of node1 and node2
     Remark: node1 and node2 must have the same CGNS label
@@ -142,13 +135,13 @@ def intersection(node1, node2, copy=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     if copy:
-        intersect_nodes = cp.deepcopy(node1)
+        intersect_nodes = PT.deep_copy(node1)
     else:
         intersect_nodes = node1
     _rm_not_common_children(intersect_nodes, node2)
     return intersect_nodes
 
-def diff(node1, node2, copy=False):
+def diff(node1, node2, copy=True):
     """
     Return a new node that correspond to node1 without node2's nodes
     Remark: node1 and node2 must have the same CGNS label
@@ -186,7 +179,7 @@ def diff(node1, node2, copy=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     if copy:
-        diff_nodes = cp.deepcopy(node1)
+        diff_nodes = PT.deep_copy(node1)
     else:
         diff_nodes = node1
     _rm_common_children(diff_nodes, node2)
