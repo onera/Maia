a3031960237ed2ab1a7bc1a1a493b46d98362534
Sebastien Bourasseau <sebastien.bourasseau@onera.fr>
2024-04-04 15:12:02 +0200

    [Pytree] Logical op - add combinated logical tests

--- maia/pytree/test/test_logical_op.py
+++ maia/pytree/test/test_logical_op.py
@@ -245,3 +245,62 @@ def test_diff():
   assert PT.is_same_tree(diff_tree2ab, ref_diff_tree2ab)
   diff_tree2ba = PLO.diff(tree2b, tree2a)
   assert PT.is_same_tree(diff_tree2ba, ref_diff_tree2ba)
+
+
+def test_combination1():
+  # As reminder in logical operations, we have:
+  # a-b = a-intersect(a,b) = union(a,b)-b
+  
+  tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
+  tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
+  
+  diff_tree1ab              = PLO.diff(        tree1a,        tree1b,            copy=True)
+  intersect_tree1ab         = PLO.intersection(tree1a,        tree1b,            copy=True)
+  union_tree1ab             = PLO.union(       tree1a,        tree1b,            copy=True)
+  diff_tree1a_intersect1ab  = PLO.diff(        tree1a,        intersect_tree1ab, copy=True)
+  diff_union_tree1ab_tree1b = PLO.diff(        union_tree1ab, tree1b,            copy=True)
+  
+  assert PT.is_same_tree(diff_tree1ab, diff_tree1a_intersect1ab)
+  assert PT.is_same_tree(diff_tree1ab, diff_union_tree1ab_tree1b)
+  
+  tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
+  tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
+  
+  diff_tree2ab              = PLO.diff(        tree2a,        tree2b,            copy=True)
+  intersect_tree2ab         = PLO.intersection(tree2a,        tree2b,            copy=True)
+  union_tree2ab             = PLO.union(       tree2a,        tree2b,            copy=True)
+  diff_tree2a_intersect2ab  = PLO.diff(        tree2a,        intersect_tree2ab, copy=True)
+  diff_union_tree2ab_tree2b = PLO.diff(        union_tree2ab, tree2b,            copy=True)
+  
+  assert PT.is_same_tree(diff_tree2ab, diff_tree2a_intersect2ab)
+  assert PT.is_same_tree(diff_tree2ab, diff_union_tree2ab_tree2b)
+  
+
+
+def test_combination2():
+  # As reminder in logical operations, we have:
+  # union(a,b) = union(union(a-b, b-a),intersect(a,b))
+  
+  tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
+  tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
+  
+  union_tree1ab                = PLO.union(       tree1a,                tree1b,            copy=True)
+  diff_tree1ab                 = PLO.diff(        tree1a,                tree1b,            copy=True)
+  diff_tree1ba                 = PLO.diff(        tree1b,                tree1a,            copy=True)
+  union_diff1ab_diff1ba        = PLO.union(       diff_tree1ab,          diff_tree1ba,      copy=True)
+  intersect_tree1ab            = PLO.intersection(tree1a,                tree1b,            copy=True)
+  union_uniond1abd1ba_inter1ab = PLO.union(       union_diff1ab_diff1ba, intersect_tree1ab, copy=True)
+  
+  assert PT.is_same_tree(union_tree1ab, union_uniond1abd1ba_inter1ab)
+  
+  tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
+  tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
+  
+  union_tree2ab                = PLO.union(       tree2a,                tree2b,            copy=True)
+  diff_tree2ab                 = PLO.diff(        tree2a,                tree2b,            copy=True)
+  diff_tree2ba                 = PLO.diff(        tree2b,                tree2a,            copy=True)
+  union_diff2ab_diff2ba        = PLO.union(       diff_tree2ab,          diff_tree2ba,      copy=True)
+  intersect_tree2ab            = PLO.intersection(tree2a,                tree2b,            copy=True)
+  union_uniond2abd2ba_inter2ab = PLO.union(       union_diff2ab_diff2ba, intersect_tree2ab, copy=True)
+  
+  assert PT.is_same_tree(union_tree2ab, union_uniond2abd2ba_inter2ab)
