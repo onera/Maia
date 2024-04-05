9f772f83c9ea53ef59ccdf012e177e43e9e0fef6
Sebastien Bourasseau <sebastien.bourasseau@onera.fr>
2024-04-05 11:40:15 +0200

    [Pytree] Logical - op Add a lambda function to compare nodes in args

--- maia/pytree/logical_op.py
+++ maia/pytree/logical_op.py
@@ -11,59 +11,48 @@ def _add_children_to_node_from_another(node1, node2):
         else:
             _add_children_to_node_from_another(child1, child2)
 
-def _rm_not_common_children(node1, node2, check_label, check_value):
+def _rm_not_common_children(node1, node2, comp_func):
     child1_to_del = []
     for child1 in PT.get_children(node1):
         child1_name = PT.get_name(child1)
         child2 = PT.get_child_from_name(node2, child1_name)
         if child2 is None:
             child1_to_del.append(child1_name)
-        elif check_label and not(PTC.is_same_label(child1,child2)):
-            child1_to_del.append(child1_name)
-        elif check_value and not(PTC.is_same_value(child1,child2)):
+        elif not(comp_func(child1,child2)):
             child1_to_del.append(child1_name)
     for child_name in child1_to_del:
         PT.rm_node_from_path(node1, child_name)
     for child1 in PT.get_children(node1):
         child2 = PT.get_child_from_name(node2, PT.get_name(child1))
         if child2 is not None:
-            _rm_not_common_children(child1, child2, check_label, check_value)
+            _rm_not_common_children(child1, child2, comp_func)
             
-def _have_common_children(node1, node2, check_label, check_value):
+def _have_common_children(node1, node2, comp_func):
     for child1 in PT.get_children(node1):
         child2 = PT.get_child_from_name(node2, PT.get_name(child1))
         if child2 is None:
             return False
-        elif check_label and not(PTC.is_same_label(child1,child2)):
-            return False
-        elif check_value and not(PTC.is_same_value(child1,child2)):
+        elif not(comp_func(child1,child2)):
             return False
         else:
-            if not _have_common_children(child1, child2, check_label, check_value):
+            if not _have_common_children(child1, child2, comp_func):
                 return False
     return True
 
-def _rm_common_children(node1, node2, check_label, check_value):
+def _rm_common_children(node1, node2, comp_func):
     child1_to_del = []
     for child1 in PT.get_children(node1):
         child1_name = PT.get_name(child1)
         child2 = PT.get_child_from_name(node2, child1_name)
         if child2 is not None:
-            delete_child = np.zeros(3,dtype=np.int32)
-            if _have_common_children(child1, child2, check_label, check_value):
-                delete_child[0] = 1
-            if (not check_label) or (check_label and PTC.is_same_label(child1,child2)):
-                delete_child[1] = 1
-            if (not check_value) or (check_value and PTC.is_same_value(child1,child2)):
-                delete_child[2] = 1
-            if (delete_child == [1,1,1]).all():
+            if _have_common_children(child1, child2, comp_func) and comp_func(child1,child2):
                 child1_to_del.append(child1_name)
     for child_name in child1_to_del:
         PT.rm_node_from_path(node1, child_name)
     for child1 in PT.get_children(node1):
         child2 = PT.get_child_from_name(node2, PT.get_name(child1))
         if child2 is not None:
-            _rm_common_children(child1, child2, check_label, check_value)
+            _rm_common_children(child1, child2, comp_func)
 
 def union(node1, node2):
     """
@@ -73,10 +62,10 @@ def union(node1, node2):
             we keep the node from node1
 
     Args:
-      node1 (CGNSNode): First CGNS node
-      node2 (CGNSNode): Second CGNS node
+      node1 (CGNSTree): First CGNS node
+      node2 (CGNSTree): Second CGNS node
     Returns:
-      CGNSNode: union of nodes
+      CGNSTree: union of nodes
     Example:
       >>> tree1 = PT.yaml.parse_yaml_cgns.to_cgns_tree('''
       ... CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
@@ -112,17 +101,18 @@ def union(node1, node2):
     _add_children_to_node_from_another(union_nodes, node2)
     return union_nodes
 
-def intersection(node1, node2, check_label=False, check_value=False):
+def intersection(node1, node2, comp_func=lambda n1,n2: PTC.is_same_node(n1,n2)):
     """
     Return a new node intersection of node1 and node2
     Remark: node1 and node2 must have the same CGNS label
     Remark: if the intersection is empty, return only the CGNSTree_t node
 
     Args:
-      node1 (CGNSNode): First CGNS node
-      node2 (CGNSNode): Second CGNS node
+      node1 (CGNSTree): First CGNS node
+      node2 (CGNSTree): Second CGNS node
+      comp_func (Callable): Fonction to compare node1 to node2
     Returns:
-      CGNSNode: intersection of nodes
+      CGNSTree: intersection of nodes
     Example:
       >>> tree1 = PT.yaml.parse_yaml_cgns.to_cgns_tree('''
       ... CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
@@ -149,20 +139,21 @@ def intersection(node1, node2, check_label=False, check_value=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     intersect_nodes = PT.shallow_copy(node1)
-    _rm_not_common_children(intersect_nodes, node2, check_label, check_value)
+    _rm_not_common_children(intersect_nodes, node2, comp_func)
     return intersect_nodes
 
-def diff(node1, node2, check_label=False, check_value=False):
+def diff(node1, node2, comp_func=lambda n1,n2: PTC.is_same_node(n1,n2)):
     """
     Return a new node that correspond to node1 without node2's nodes
     Remark: node1 and node2 must have the same CGNS label
     Remark: if the intersection is empty, return only the CGNSTree_t node
 
     Args:
-      node1 (CGNSNode): First CGNS node
-      node2 (CGNSNode): Second CGNS node
+      node1 (CGNSTree): First CGNS node
+      node2 (CGNSTree): Second CGNS node
+      comp_func (Callable): Fonction to compare node1 to node2
     Returns:
-      CGNSNode: differences between nodes
+      CGNSTree: differences between nodes
     Example:
       >>> tree1 = PT.yaml.parse_yaml_cgns.to_cgns_tree('''
       ... CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
@@ -190,5 +181,5 @@ def diff(node1, node2, check_label=False, check_value=False):
     if PT.get_label(node1) != PT.get_label(node2):
         raise TypeError(f"{PT.get_name(node1)} and {PT.get_name(node2)} have different CGNS labels ({PT.get_label(node1)} vs {PT.get_label(node2)})")
     diff_nodes = PT.shallow_copy(node1)
-    _rm_common_children(diff_nodes, node2, check_label, check_value)
+    _rm_common_children(diff_nodes, node2, comp_func)
     return diff_nodes
--- maia/pytree/test/test_logical_op.py
+++ maia/pytree/test/test_logical_op.py
@@ -2,6 +2,7 @@ import numpy  as np
 import pytest
 
 import maia.pytree            as PT
+import maia.pytree.compare    as PTC
 import maia.pytree.logical_op as PLO
 
 from maia.pytree.yaml import parse_yaml_cgns
@@ -268,7 +269,7 @@ def test_label_differ():
     diff_tree = PT.deep_copy(PLO.diff(tree1, tree2))
 
 
-def test_with_check():
+def test_comp_func():
   yt1 = """
   CGNSLibraryVersion CGNSLibraryVersion_t R4 [4.2]:
   BaseA CGNSBase_t:
@@ -294,41 +295,42 @@ def test_with_check():
   ref_diff_tree = PT.shallow_copy(tree1)
   PT.rm_node_from_path(ref_diff_tree, 'CGNSLibraryVersion')
   
-  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2))
+  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_name(n1,n2)))
   assert PT.is_same_tree(intersect_tree, tree1)
   
-  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2, check_label=True))
-  PT.print_tree(intersect_tree)
+  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_label(n1,n2)))
   assert PT.is_same_tree(intersect_tree, ref_intersect_tree)
   
-  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2, check_value=True))
+  intersect_tree = PT.deep_copy(PLO.intersection(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_value(n1,n2)))
   assert PT.is_same_tree(intersect_tree, ref_intersect_tree)
   
   
-  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2))
+  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_name(n1,n2)))
   assert PT.is_same_tree(diff_tree, root_tree)
   
-  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2, check_label=True))
+  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_label(n1,n2)))
   assert PT.is_same_tree(diff_tree, ref_diff_tree)
   
-  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2, check_value=True))
+  diff_tree = PT.deep_copy(PLO.diff(tree1, tree2, comp_func=lambda n1,n2: PTC.is_same_value(n1,n2)))
   assert PT.is_same_tree(diff_tree, ref_diff_tree)
 
 
-@pytest.mark.parametrize("CHECK_LABEL", [False, True])
-@pytest.mark.parametrize("CHECK_VALUE", [False, True])
-def test_combination1(CHECK_LABEL,CHECK_VALUE):
+@pytest.mark.parametrize("COMP_FUNC", [lambda n1,n2: PTC.is_same_node(n1,n2), 
+                                       lambda n1,n2: PTC.is_same_name(n1,n2),
+                                       lambda n1,n2: PTC.is_same_label(n1,n2),
+                                       lambda n1,n2: PTC.is_same_value(n1,n2)])
+def test_combination1(COMP_FUNC):
   # As reminder, in logical operations, we have:
   # a-b = a-intersect(a,b) = union(a,b)-b
   
   tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
   tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
   
-  diff_tree1ab              = PT.deep_copy(PLO.diff(        tree1a,        tree1b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  intersect_tree1ab         = PT.deep_copy(PLO.intersection(tree1a,        tree1b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_tree1ab             = PT.deep_copy(PLO.union(       tree1a,        tree1b                                                             ))
-  diff_tree1a_intersect1ab  = PT.deep_copy(PLO.diff(        tree1a,        intersect_tree1ab, check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  diff_union_tree1ab_tree1b = PT.deep_copy(PLO.diff(        union_tree1ab, tree1b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
+  diff_tree1ab              = PT.deep_copy(PLO.diff(        tree1a,        tree1b,            comp_func=COMP_FUNC))
+  intersect_tree1ab         = PT.deep_copy(PLO.intersection(tree1a,        tree1b,            comp_func=COMP_FUNC))
+  union_tree1ab             = PT.deep_copy(PLO.union(       tree1a,        tree1b                                ))
+  diff_tree1a_intersect1ab  = PT.deep_copy(PLO.diff(        tree1a,        intersect_tree1ab, comp_func=COMP_FUNC))
+  diff_union_tree1ab_tree1b = PT.deep_copy(PLO.diff(        union_tree1ab, tree1b,            comp_func=COMP_FUNC))
   
   assert PT.is_same_tree(diff_tree1ab, diff_tree1a_intersect1ab)
   assert PT.is_same_tree(diff_tree1ab, diff_union_tree1ab_tree1b)
@@ -336,44 +338,46 @@ def test_combination1(CHECK_LABEL,CHECK_VALUE):
   tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
   tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
   
-  diff_tree2ab              = PT.deep_copy(PLO.diff(        tree2a,        tree2b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  intersect_tree2ab         = PT.deep_copy(PLO.intersection(tree2a,        tree2b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_tree2ab             = PT.deep_copy(PLO.union(       tree2a,        tree2b                                                             ))
-  diff_tree2a_intersect2ab  = PT.deep_copy(PLO.diff(        tree2a,        intersect_tree2ab, check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  diff_union_tree2ab_tree2b = PT.deep_copy(PLO.diff(        union_tree2ab, tree2b,            check_label=CHECK_LABEL, check_value=CHECK_VALUE))
+  diff_tree2ab              = PT.deep_copy(PLO.diff(        tree2a,        tree2b,            comp_func=COMP_FUNC))
+  intersect_tree2ab         = PT.deep_copy(PLO.intersection(tree2a,        tree2b,            comp_func=COMP_FUNC))
+  union_tree2ab             = PT.deep_copy(PLO.union(       tree2a,        tree2b                                ))
+  diff_tree2a_intersect2ab  = PT.deep_copy(PLO.diff(        tree2a,        intersect_tree2ab, comp_func=COMP_FUNC))
+  diff_union_tree2ab_tree2b = PT.deep_copy(PLO.diff(        union_tree2ab, tree2b,            comp_func=COMP_FUNC))
   
   assert PT.is_same_tree(diff_tree2ab, diff_tree2a_intersect2ab)
   assert PT.is_same_tree(diff_tree2ab, diff_union_tree2ab_tree2b)
   
 
 
-@pytest.mark.parametrize("CHECK_LABEL", [False, True])
-@pytest.mark.parametrize("CHECK_VALUE", [False, True])
-def test_combination2(CHECK_LABEL,CHECK_VALUE):
+@pytest.mark.parametrize("COMP_FUNC", [lambda n1,n2: PTC.is_same_node(n1,n2), 
+                                       lambda n1,n2: PTC.is_same_name(n1,n2),
+                                       lambda n1,n2: PTC.is_same_label(n1,n2),
+                                       lambda n1,n2: PTC.is_same_value(n1,n2)])
+def test_combination2(COMP_FUNC):
   # As reminder, in logical operations, we have:
   # union(a,b) = union(union(a-b, b-a),intersect(a,b))
   
   tree1a = parse_yaml_cgns.to_cgns_tree(yt1a)
   tree1b = parse_yaml_cgns.to_cgns_tree(yt1b)
   
-  union_tree1ab                = PT.deep_copy(PLO.union(       tree1a,                tree1b                                                            ))
-  diff_tree1ab                 = PT.deep_copy(PLO.diff(        tree1a,                tree1b,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  diff_tree1ba                 = PT.deep_copy(PLO.diff(        tree1b,                tree1a,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_diff1ab_diff1ba        = PT.deep_copy(PLO.union(       diff_tree1ab,          diff_tree1ba                                                      ))
-  intersect_tree1ab            = PT.deep_copy(PLO.intersection(tree1a,                tree1b,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_uniond1abd1ba_inter1ab = PT.deep_copy(PLO.union(       union_diff1ab_diff1ba, intersect_tree1ab                                                 ))
+  union_tree1ab                = PT.deep_copy(PLO.union(       tree1a,                tree1b                               ))
+  diff_tree1ab                 = PT.deep_copy(PLO.diff(        tree1a,                tree1b,           comp_func=COMP_FUNC))
+  diff_tree1ba                 = PT.deep_copy(PLO.diff(        tree1b,                tree1a,           comp_func=COMP_FUNC))
+  union_diff1ab_diff1ba        = PT.deep_copy(PLO.union(       diff_tree1ab,          diff_tree1ba                         ))
+  intersect_tree1ab            = PT.deep_copy(PLO.intersection(tree1a,                tree1b,           comp_func=COMP_FUNC))
+  union_uniond1abd1ba_inter1ab = PT.deep_copy(PLO.union(       union_diff1ab_diff1ba, intersect_tree1ab                    ))
   
   assert PT.is_same_tree(union_tree1ab, union_uniond1abd1ba_inter1ab)
   
   tree2a = parse_yaml_cgns.to_cgns_tree(yt2a)
   tree2b = parse_yaml_cgns.to_cgns_tree(yt2b)
   
-  union_tree2ab                = PT.deep_copy(PLO.union(       tree2a,                tree2b                                                            ))
-  diff_tree2ab                 = PT.deep_copy(PLO.diff(        tree2a,                tree2b,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  diff_tree2ba                 = PT.deep_copy(PLO.diff(        tree2b,                tree2a,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_diff2ab_diff2ba        = PT.deep_copy(PLO.union(       diff_tree2ab,          diff_tree2ba                                                      ))
-  intersect_tree2ab            = PT.deep_copy(PLO.intersection(tree2a,                tree2b,           check_label=CHECK_LABEL, check_value=CHECK_VALUE))
-  union_uniond2abd2ba_inter2ab = PT.deep_copy(PLO.union(       union_diff2ab_diff2ba, intersect_tree2ab                                                 ))
+  union_tree2ab                = PT.deep_copy(PLO.union(       tree2a,                tree2b                               ))
+  diff_tree2ab                 = PT.deep_copy(PLO.diff(        tree2a,                tree2b,           comp_func=COMP_FUNC))
+  diff_tree2ba                 = PT.deep_copy(PLO.diff(        tree2b,                tree2a,           comp_func=COMP_FUNC))
+  union_diff2ab_diff2ba        = PT.deep_copy(PLO.union(       diff_tree2ab,          diff_tree2ba                         ))
+  intersect_tree2ab            = PT.deep_copy(PLO.intersection(tree2a,                tree2b,           comp_func=COMP_FUNC))
+  union_uniond2abd2ba_inter2ab = PT.deep_copy(PLO.union(       union_diff2ab_diff2ba, intersect_tree2ab                    ))
   
   assert PT.is_same_tree(union_tree2ab, union_uniond2abd2ba_inter2ab)
   
