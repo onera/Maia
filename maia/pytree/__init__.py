e5888bd011f8b8bf9761cce7c371380963fbab7d
Julien Coulet <julien.coulet@onera.fr>
2024-04-12 11:11:52 +0200

    Fixup imports (expose yaml only if installed)

--- maia/pytree/__init__.py
+++ maia/pytree/__init__.py
@@ -2,4 +2,12 @@ from .sids import *
 from .walk import *
 from .node import *
 
-from .compare       import *
+from .compare import *
+
+from . import utils
+
+# Optional modules
+try:
+  from . import yaml
+except ModuleNotFoundError:
+  pass
