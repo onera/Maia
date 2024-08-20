52813723d9ac54c7b3a8579bce7df909e74d81e3
Julien Coulet <julien.coulet@onera.fr>
2024-08-20 18:26:14 +0200

    [ngon->elt] Remove old implementation

--- maia/algo/dist/ngons_to_elements.py
+++ maia/algo/dist/ngons_to_elements.py
@@ -1,84 +1,20 @@
-from mpi4py import MPI
 import numpy as np
 
-from cmaia import dist_algo as cdist_algo
-from maia.algo.apply_function_to_nodes import apply_to_zones
-
 import maia
-import maia.pytree as PT
-from maia.utils import require_cpp20
 
-@require_cpp20
-def ngons_to_elements(t,comm):
-  """
-  Transform a polyedric (NGon) based connectivity into a standard nodal
-  connectivity.
-  
-  Tree is modified in place : polyedric element, which are supposed to describe
-  only standard elements (tris, quads, tets, pyras, prisms and hexa)
-  are removed from the zones and Pointlist (under the BC_t nodes) are updated.
+import maia.pytree      as PT
+import maia.pytree.maia as MT
+from maia.pytree.sids import elements_utils as EU
 
-  Warning: 
-    This function has not been parallelized yet. Tree is internally gathered
-    to a single process, which can cause memory or performance issues on large cases.
+from maia.utils     import np_utils, par_utils
+from maia.transfer  import protocols as EP
+from maia.algo.dist import matching_jns_tools as MJT
 
-  Args:
-    disttree   (CGNSTree): Tree with connectivity described by NGons
-    comm       (`MPIComm`) : MPI communicator
 
-  Example:
-      .. literalinclude:: snippets/test_algo.py
-        :start-after: #ngons_to_elements@start
-        :end-before: #ngons_to_elements@end
-        :dedent: 2
-  """
-  # Function require NFACE + NGON with PE
-  for zone in PT.get_all_Zone_t(t):
-    if not PT.Zone.has_nface_elements(zone):
-      maia.algo.pe_to_nface(zone, comm)
-    ng = PT.Zone.NGonNode(zone)
-    if PT.get_child_from_name(ng, 'ParentElements') is None:
-      maia.algo.nface_to_pe(zone, comm)
-
-  if comm.Get_size() > 1:
-    maia.algo.dist.redistribute_tree(t, 'gather.0', comm)
-  
-  zone_to_elts = {}
-  if comm.Get_rank() == 0:
-    apply_to_zones(cdist_algo.convert_zone_to_std_elements, t)
-
-    # This is to broadcast new element names to other ranks
-    for zone_path in PT.predicates_to_paths(t, 'CGNSBase_t/Zone_t'):
-      zone = PT.get_node_from_path(t, zone_path)
-      elts = PT.get_children_from_label(zone, 'Elements_t')
-      zone_to_elts[zone_path] = []
-      for elt in elts:
-        void_elt = PT.shallow_copy(elt)
-        cnt = PT.get_child_from_name(void_elt, 'ElementConnectivity')
-        cnt[1] = np.empty_like(cnt[1], shape=(0,))
-        zone_to_elts[zone_path].append(void_elt)
-
-  if comm.Get_size() > 1:
-    zone_to_elts = comm.bcast(zone_to_elts, root=0)
-    if comm.Get_rank() != 0:
-      for zone_path, elts in zone_to_elts.items():
-        zone = PT.get_node_from_path(t, zone_path)
-        PT.rm_nodes_from_label(t, 'Elements_t')
-        for elt in elts:
-          distri = PT.maia.getDistribution(elt, 'Element')[1]
-          distri[:] = distri[2]
-          PT.add_child(zone, elt)
-
-    maia.algo.dist.redistribute_tree(t, 'uniform', comm)
-
+from cmaia.dist_algo import combine_to_tetra, combine_to_pyra, \
+                            combine_to_penta, combine_to_hexa
 
 def _ngon_to_elt_zone(zone, comm):
-  import maia.pytree.maia as MT
-  from maia.utils import np_utils, par_utils
-  from maia.transfer import protocols as EP
-  from cmaia.dist_algo import combine_to_tetra, combine_to_pyra, \
-                              combine_to_penta, combine_to_hexa
-  from maia.pytree.sids import elements_utils as EU
 
   # Start by constructing boundary faces
   ngon_n = PT.Zone.NGonNode(zone)
@@ -224,10 +160,36 @@ def _ngon_to_elt_zone(zone, comm):
   PT.rm_child(zone, ngon_n)
   PT.rm_child(zone, nface_n)
  
-def ngon_to_elt(dist_tree, comm):
-  
-  from maia.algo.dist import matching_jns_tools as MJT
 
+def ngons_to_elements(dist_tree, comm):
+  """
+  Transform a polyedric (NGon based) connectivity into a standard nodal
+  connectivity.
+  
+  Tree is modified in place : polyedric element, which are supposed to describe
+  only standard elements (tris, quads, tets, pyras, prisms and hexa)
+  are removed and relevant data (such as PointList) are updated.
+
+  Args:
+    dist_tree  (CGNSTree): Tree with connectivity described by NGons
+    comm       (`MPIComm`) : MPI communicator
+
+  Example:
+      .. literalinclude:: snippets/test_algo.py
+        :start-after: #ngons_to_elements@start
+        :end-before: #ngons_to_elements@end
+        :dedent: 2
+  """
+
+  # Function require NFACE + NGON with PE
+  for zone in PT.get_all_Zone_t(dist_tree):
+    if not PT.Zone.has_nface_elements(zone):
+      maia.algo.pe_to_nface(zone, comm)
+    ng = PT.Zone.NGonNode(zone)
+    if PT.get_child_from_name(ng, 'ParentElements') is None:
+      maia.algo.nface_to_pe(zone, comm)
+
+  # Needed to update the joins afterward
   MJT.add_joins_donor_name(dist_tree, comm)
 
   for zone in PT.get_all_Zone_t(dist_tree):
---
+++ maia/algo/dist/test/test_ngons_to_elements.py
@@ -0,0 +1,68 @@
+import pytest
+import pytest_parallel
+import os
+import numpy as np
+
+import maia
+import maia.pytree as PT
+
+from maia.utils import test_utils as TU
+
+from maia.algo.dist.ngons_to_elements import _ngon_to_elt_zone, ngons_to_elements
+
+@pytest_parallel.mark.parallel(1)
+def test_basic(comm):
+
+  filename = os.path.join(TU.mesh_dir, 'hex_2_prism_2.yaml')
+
+  tree = maia.io.file_to_dist_tree(filename, comm)
+  zone = PT.get_node_from_label(tree, 'Zone_t')
+  maia.algo.dist.convert_elements_to_ngon(tree, comm) # Note: we are not testing that, its just a way to get an ngon test
+
+  _ngon_to_elt_zone(zone, comm) # apply tested function
+
+  # Checks
+  expected_range = [[1,2],    # TRI_3
+                    [3,14],   # QUAD_4
+                    [15,16],  # PENTA_6
+                    [17,18]]  # HEXA_8
+  expected_ec = [[5,3,2,13,15,12],
+                 [4,5,2,1,  2,7,6,1,  6,9,4,1,  8,7,2,3,  10,8,3,5,  9,10,5,4,  7,12,11,6,  11,14,9,6,  13,12,7,8,  15,13,8,10,  14,15,10,9,  15,14,11,12],
+                 [2,3,5,7,8,10,  8,10,7,13,15,12],
+                 [1,2,5,4,6,7,10,9,  10,9,6,7,15,14,11,12]]
+
+  for i,kind in enumerate(['TRI_3', 'QUAD_4', 'PENTA_6', 'HEXA_8']):
+    elt = PT.get_child_from_name(zone, kind)
+    assert PT.Element.CGNSName(elt) == kind
+    assert np.array_equal(PT.get_child_from_name(elt, "ElementRange")[1], expected_range[i])
+    assert np.array_equal(PT.get_child_from_name(elt, "ElementConnectivity")[1], expected_ec[i])
+
+
+def test_all_kinds(comm):
+  filename = os.path.join(TU.mesh_dir, 'hex_prism_pyra_tet.yaml')
+
+  tree = maia.io.file_to_dist_tree(filename, comm)
+  zone = PT.get_node_from_label(tree, 'Zone_t')
+  maia.algo.dist.convert_elements_to_ngon(tree, comm) # Note: we are not testing that, its just a way to get an ngon test
+
+  _ngon_to_elt_zone(zone, comm) # apply tested function
+
+   # Checks
+  expected_range = [[1,6],    # TRI_3
+                    [7,12],   # QUAD_4
+                    [13,13],  # TETRA_4
+                    [14,14],  # PYRA_5
+                    [15,15],  # PENTA_6
+                    [16,16]]  # HEXA_8
+  expected_ec = [[5,3,2,  11,6,7,  11,9,6,  7,8,11,  8,10,11,  11,10,9],
+                 [4,5,2,1,  2,7,6,1,  1,6,9,4,  8,7,2,3,  3,5,10,8,  9,10,5,4],
+                 [11,7,10,8],
+                 [10,9,6,7,11],
+                 [2,3,5,7,8,10,],
+                 [1,2,5,4,6,7,10,9]]
+
+  for i,kind in enumerate(['TRI_3', 'QUAD_4', 'TETRA_4', 'PYRA_5', 'PENTA_6', 'HEXA_8']):
+    elt = PT.get_child_from_name(zone, kind)
+    assert PT.Element.CGNSName(elt) == kind
+    assert np.array_equal(PT.get_child_from_name(elt, "ElementRange")[1], expected_range[i])
+    assert np.array_equal(PT.get_child_from_name(elt, "ElementConnectivity")[1], expected_ec[i]) # Checks
