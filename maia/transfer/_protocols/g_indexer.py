6fa24a21b856cb242c8abede7d0bf0e1b133fc90
Julien Coulet <julien.coulet@onera.fr>
2024-09-27 17:13:54 +0200

    Manage multiple g_idx case

--- maia/transfer/_protocols/g_indexer.py
+++ maia/transfer/_protocols/g_indexer.py
@@ -7,6 +7,28 @@ def counting_sort(array, n_bins):
   from cmaia.utils import layouts
   return layouts.counting_sort(array, n_bins)
 
+def counting_sort_mult(arrays, n_bins):
+  from cmaia.utils import layouts
+  return layouts.counting_sort_mult(arrays, n_bins)
+  counts = np.zeros(n_bins, int)
+  for array in arrays:
+    counts += np.bincount(array, minlength=n_bins)
+
+  displ = np.empty(counts.size+1, int)
+  displ[0] = 0
+  np.cumsum(counts, out=displ[1:])
+
+  counts *= 0
+  out_list = list()
+  for array in arrays:
+    out = np.empty_like(array)
+    for i in range(array.size):
+      out[i] = displ[array[i]] + counts[array[i]]
+      counts[array[i]] += 1
+    out_list.append(out)
+
+  return out_list, counts
+
 def take_strided(a_counts, a_val, indices, out):
   """
   An equivalent to numpy.take (a[ind]), but with strided values in a.
@@ -33,18 +55,18 @@ def put_strided(a, indices, indices_count, read_counts, read):
 
 class GIndexer:
 
-  def __init__(self, distri, g_idx, comm):
+  def __init__(self, distri, g_idx_l, comm):
     assert distri.size == comm.Get_size() + 1
 
     # Binary search : for each gnum, find the corresponding rank in distribution
-    owning_rank = np.searchsorted(distri, g_idx-1, side='right') - 1
+    owning_rank_l = [np.searchsorted(distri, g_idx-1, side='right') - 1 for g_idx in g_idx_l]
 
-    ok = (0 <= owning_rank).all() and (owning_rank < comm.Get_size()).all()
+    ok = all( (0 <= owning_rank).all() and (owning_rank < comm.Get_size()).all() for owning_rank in owning_rank_l)
     if not comm.allreduce(ok, op=MPI.LAND):
       raise IndexError("Some idx not in distri")
 
     # Compute the sorting order to put lngn in increasing rank order
-    sorting_idx, send_counts = counting_sort(owning_rank, comm.Get_size())
+    sorting_idx_l, send_counts = counting_sort_mult(owning_rank_l, comm.Get_size())
 
     # Now send gnum to their managing rank, and move it to local indices
     recv_counts = np.empty(comm.Get_size(), int)
@@ -53,17 +75,18 @@ class GIndexer:
     send_data = np.empty(send_counts.sum(), int)
     recv_data = np.empty(recv_counts.sum(), int)
 
-    send_data[sorting_idx] = g_idx
+    for g_idx, sorting_idx in zip(g_idx_l, sorting_idx_l):
+      send_data[sorting_idx] = g_idx
     comm.Alltoallv((send_data, send_counts), (recv_data, recv_counts))
     recv_data -= (distri[comm.Get_rank()] + 1)
 
     self.comm = comm
     self.dn = distri[comm.Get_rank()+1] - distri[comm.Get_rank()]   # Number of managed indices
-    self.pn = g_idx.size                                            # Number of accessed indices
+    self.pn = [g_idx.size for g_idx in g_idx_l]                     # Number of accessed indices
     self.dist_counts = recv_counts  # Number of managed indices accessed by each rank (with multiplicity)
     self.part_counts = send_counts  # Number of accessed indices managed by each rank (with multiplicity)
     self.dist_select_idx = recv_data   # Selection order of managed data to put it in MPI order
-    self.part_write_pos  = sorting_idx # Position where accessed data should be put to have it in MPI order
+    self.part_write_pos  = sorting_idx_l # Position where accessed data should be put to have it in MPI order
 
     self._empty_dist = (np.diff(distri)==0).any() # True if at least one rank has dn == 0
     self._empty_part = None                       # True if at least one rank has pn == 0
@@ -74,27 +97,31 @@ class GIndexer:
   @property
   def empty_part(self):
     if self._empty_part is None:
-      self._empty_part = self.comm.allreduce(self.pn == 0, MPI.LOR)
+      is_empty = not any(pn > 0 for pn in self.pn)
+      self._empty_part = self.comm.allreduce(is_empty, MPI.LOR)
     return self._empty_part
 
 
-  def Take_into(self, data_in, data_out):
+  def Take_into(self, data_in, data_out_l):
     """
     AlltoAll like with constant (guessed) counts
     """
+    assert len(data_out_l) == len(self.pn)
     counts_in  = data_in.size  // self.dn if self.dn != 0 else 0
-    counts_out = data_out.size // self.pn if self.pn != 0 else 0
+    counts_out = 0 # Init for procs having n_part == 0
 
     if data_in.size - counts_in*self.dn != 0:
       raise ValueError("Input data size is not a multiple of managed idx")
-    if data_out.size - counts_out*self.pn != 0:
-      raise ValueError("Output data size is not a multiple of requested idx")
-    if counts_in != counts_out and counts_in*counts_out != 0:
-      raise ValueError("Input and output counts does not match")
+    for data_out, pn in zip(data_out_l, self.pn):
+      counts_out = data_out.size // pn if pn != 0 else 0
+      if data_out.size - counts_out*pn != 0:
+        raise ValueError("Output data size is not a multiple of requested idx")
+      if counts_in != counts_out and counts_in*counts_out != 0:
+        raise ValueError("Input and output counts does not match")
 
 
     send_buff = np.empty(counts_in*self.dist_select_idx.size, data_in.dtype)
-    recv_buff = np.empty(counts_out*self.pn,                  data_out.dtype)
+    recv_buff = np.empty(counts_out*sum(self.pn),             data_in.dtype)
 
     if counts_in == 1:
       send_buff[:] = data_in[self.dist_select_idx]
@@ -107,28 +134,30 @@ class GIndexer:
                         (recv_buff, counts_out*self.part_counts))
 
     # Data has been received in owning proc order : 'unsort' it to recover lngn ordering
-    if counts_out == 1:
-      data_out[:] = recv_buff[self.part_write_pos]
-    else:
-      put_idx = counts_out*self.part_write_pos
-      for j in range(counts_out):
-        data_out[j::counts_out] = recv_buff[put_idx+j]
+    for data_out, part_write_pos in zip(data_out_l, self.part_write_pos):
+      if counts_out == 1:
+        data_out[:] = recv_buff[part_write_pos]
+      else:
+        put_idx = counts_out*part_write_pos
+        for j in range(counts_out):
+          data_out[j::counts_out] = recv_buff[put_idx+j]
 
   def Take(self, data_in):
     counts = data_in.size // self.dn if self.dn != 0 else 0
     if self.empty_dist:
       counts = self.comm.allreduce(counts, MPI.MAX)
 
-    data_out = np.empty(counts*self.pn, data_in.dtype)
-    self.Take_into(data_in, data_out)
-    return data_out
+    data_out_l = [np.empty(counts*pn, data_in.dtype) for pn in self.pn]
+    self.Take_into(data_in, data_out_l)
+    return data_out_l
 
 
-  def Take_v_into(self, data_in, counts_in, data_out, counts_out):
+  def Take_v_into(self, data_in, counts_in, data_out_l, counts_out_l):
     # Retrive MPI order for counts
     _counts_in  = counts_in[self.dist_select_idx]
-    _counts_out = np.empty(counts_out.size, counts_out.dtype)
-    _counts_out[self.part_write_pos] = counts_out
+    _counts_out = np.empty(sum([c.size for c in counts_out_l]), counts_in.dtype)
+    for counts_out, part_write_pos in zip(counts_out_l, self.part_write_pos):
+      _counts_out[part_write_pos] = counts_out
 
     # Count the actual number of items to send/recv, using stride array
     # (this is the partial sum of portion of the stride array related to the given rank)
@@ -149,7 +178,8 @@ class GIndexer:
     self.comm.Alltoallv((send_buff, send_counts), (recv_buff, recv_counts))
 
     # Post treat recv buffer (data arrive in mpi layout, put it in requested layout)
-    take_strided(_counts_out, recv_buff, self.part_write_pos, data_out)
+    for data_out, part_write_pos in zip(data_out_l, self.part_write_pos):
+      take_strided(_counts_out, recv_buff, part_write_pos, data_out)
 
 
   def Take_v(self, data_in, counts_in):
@@ -162,9 +192,9 @@ class GIndexer:
     # Exchange counts_in
     # _ : in all_to_all layout. Do not call Take because we need the intermediate layout
     _counts_in  = counts_in[self.dist_select_idx]
-    _counts_out = np.empty(self.pn, counts_in.dtype)
+    _counts_out = np.empty(sum(self.pn), counts_in.dtype)
     self.comm.Alltoallv((_counts_in, self.dist_counts), (_counts_out, self.part_counts))
-    counts_out = _counts_out[self.part_write_pos]
+    counts_out_l = [_counts_out[part_write_pos] for part_write_pos in self.part_write_pos]
 
     # Count the actual number of items to send/recv, using stride array
     # (this is the partial sum of portion of the stride array related to the given rank)
@@ -187,11 +217,14 @@ class GIndexer:
     self.comm.Alltoallv((send_buff, send_counts), (recv_buff, recv_counts))
 
     # Post treat recv buffer (data arrive in mpi layout, put it in requested layout)
-    data_out = np.empty_like(recv_buff)
-    take_strided(_counts_out, recv_buff, self.part_write_pos, data_out)
+    data_out_l = list()
+    for counts_out, part_write_pos in zip(counts_out_l, self.part_write_pos):
+      data_out = np.empty(counts_out.sum(), recv_buff.dtype)
+      take_strided(_counts_out, recv_buff, part_write_pos, data_out)
+      data_out_l.append(data_out)
 
+    return data_out_l, counts_out_l
 
-    return data_out, counts_out
 
   def take(self, data_in):
     """
@@ -203,36 +236,44 @@ class GIndexer:
 
     joined = np.frombuffer(b''.join(pickelized), dtype=np.int8)
     
-    data_out, counts_out = self.Take_v(joined, counts_in)
+    data_out_l, counts_out_l = self.Take_v(joined, counts_in)
     
-    out = []
-    r_start = 0
-    for size in counts_out:
-      out.append(pickle.loads(data_out[r_start:r_start+size].tobytes()))
-      r_start += size
-    return out
+    res = list()
+    for data_out, counts_out in zip(data_out_l, counts_out_l):
+      out = []
+      r_start = 0
+      for size in counts_out:
+        out.append(pickle.loads(data_out[r_start:r_start+size].tobytes()))
+        r_start += size
+      res.append(out)
+    return res
 
 
-  def Put_into(self, data_in, data_out):
-    counts_in  = data_in.size  // self.pn if self.pn != 0 else 0
+  def Put_into(self, data_in_l, data_out):
+    assert len(data_in_l) == len(self.pn)
+    counts_in  = 0
     counts_out = data_out.size // self.dn if self.dn != 0 else 0
 
-    if data_in.size - counts_in*self.pn != 0:
-      raise ValueError("Input data size is not a multiple of managed idx")
     if data_out.size - counts_out*self.dn != 0:
       raise ValueError("Output data size is not a multiple of requested idx")
-    if counts_in != counts_out and counts_in*counts_out != 0:
-      raise ValueError("Input and output counts does not match")
+    for data_in, pn in zip(data_in_l, self.pn):
+      counts_in  = data_in.size  // pn if pn != 0 else 0
+      if data_in.size - counts_in*pn != 0:
+        raise ValueError("Input data size is not a multiple of managed idx")
+      if counts_in != counts_out and counts_in*counts_out != 0:
+        raise ValueError("Input and output counts does not match")
 
-    send_buff = np.empty(counts_in*self.part_write_pos.size, dtype=data_out.dtype)
-    recv_buff = np.empty(counts_out*self.dist_counts.sum(),  dtype=data_in.dtype)
 
-    if counts_in == 1:
-      send_buff[self.part_write_pos] = data_in
-    else:
-      pull_idx = counts_in*self.part_write_pos
-      for j in range(counts_in):
-        send_buff[pull_idx+j] = data_in[j::counts_in]
+    send_buff = np.empty(counts_in*sum([write_pos.size for write_pos in self.part_write_pos]), dtype=data_out.dtype)
+    recv_buff = np.empty(counts_out*self.dist_counts.sum(),  dtype=data_out.dtype)
+
+    for data_in, part_write_pos in zip(data_in_l, self.part_write_pos):
+      if counts_in == 1:
+        send_buff[part_write_pos] = data_in
+      else:
+        pull_idx = counts_in*part_write_pos
+        for j in range(counts_in):
+          send_buff[pull_idx+j] = data_in[j::counts_in]
 
     self.comm.Alltoallv((send_buff, counts_in*self.part_counts), 
                         (recv_buff, counts_out*self.dist_counts))
@@ -244,28 +285,39 @@ class GIndexer:
       for j in range(counts_out):
         data_out[put_idx+j] = recv_buff[j::counts_out]
 
-  def Put(self, data_in):
-    counts  = data_in.size  // self.pn if self.pn != 0 else 0
+  def Put(self, data_in_l):
+    assert len(data_in_l) == len(self.pn)
+    counts_l = [data_in.size // pn for data_in, pn in zip(data_in_l, self.pn) if pn != 0]
+    assert all(count == counts_l[0] for count in counts_l)
+    counts = counts_l[0]            if len(counts_l)  > 0 else 0
+    dtype  = data_in_l[0].dtype.str if len(data_in_l) > 0 else ''
     if self.empty_part:
       counts = self.comm.allreduce(counts, MPI.MAX)
+      dtype  = self.comm.allreduce(dtype,  MPI.MAX)
 
-    data_out = np.empty(counts*self.dn, data_in.dtype)
-    self.Put_into(data_in, data_out)
+    data_out = np.empty(counts*self.dn, dtype)
+    self.Put_into(data_in_l, data_out)
     return data_out
 
-  def Put_v(self, data_in, counts_in):
+  def Put_v(self, data_in_l, counts_in_l):
     # Variable stride
 
-    assert isinstance(counts_in, np.ndarray)
-    assert counts_in.size == self.pn
-    assert counts_in.dtype == int
-    assert data_in.size == counts_in.sum()
+    assert len(counts_in_l) == len(data_in_l) == len(self.pn)
+    assert all(counts_in.size == pn for counts_in,pn in zip(counts_in_l, self.pn))
+    assert all(counts_in.dtype == int for counts_in in counts_in_l)
+    assert all(data_in.size == counts_in.sum() for data_in, counts_in in zip(data_in_l, counts_in_l))
+
+    dtype  = data_in_l[0].dtype.str if len(data_in_l) > 0 else ''
+    if self.empty_part:
+      dtype = self.comm.allreduce(dtype,  MPI.MAX)
+
 
     # Exchange counts_in
     # _ : in all_to_all layout. Do not call Put because we need the intermediate layout
-    _counts_in = np.empty(self.part_write_pos.size, dtype=int)
+    _counts_in = np.empty(sum([part_write_pos.size for part_write_pos in self.part_write_pos]), dtype=int)
     _counts_out = np.empty(self.dist_counts.sum(),  dtype=int)
-    _counts_in[self.part_write_pos] = counts_in
+    for counts_in, part_write_pos in zip(counts_in_l, self.part_write_pos):
+      _counts_in[part_write_pos] = counts_in
 
     self.comm.Alltoallv((_counts_in, self.part_counts), 
                         (_counts_out, self.dist_counts))
@@ -284,34 +336,34 @@ class GIndexer:
       idx_recv += self.dist_counts[i]
 
     # Prepare send buffer (put data in alltoall layout)
-    inv_sort_idx = np.empty(self.part_write_pos.size, dtype=int)
-    inv_sort_idx[self.part_write_pos] = np.arange(self.part_write_pos.size)
-
-    send_buff = np.empty(send_counts.sum(), data_in.dtype)
-    take_strided(counts_in, data_in, inv_sort_idx, send_buff)
+    send_buff = np.empty(send_counts.sum(), dtype)
+    for i, part_write_pos in enumerate(self.part_write_pos):
+      put_strided(send_buff, part_write_pos, _counts_in, counts_in_l[i], data_in_l[i])
 
     # Exchange data buffer
-    recv_buff = np.empty(recv_counts.sum(), data_in.dtype)
+    recv_buff = np.empty(recv_counts.sum(), send_buff.dtype)
     self.comm.Alltoallv((send_buff, send_counts), (recv_buff, recv_counts))
 
     # Post treat recv buffer (data arrive in mpi layout, put it in requested layout)
-    data_out = np.empty(counts_out.sum(), data_in.dtype)
+    data_out = np.empty(counts_out.sum(), recv_buff.dtype)
     put_strided(data_out, self.dist_select_idx, counts_out, _counts_out, recv_buff)
 
     return data_out, counts_out
 
 
-  def put(self, data_in):
+  def put(self, data_in_l):
     """
     Put implementation for any python object.
     data_in must be a list of size dn on each rank.
     """
-    pickelized = [pickle.dumps(data) for data in data_in]
-    counts_in  = np.array([len(p) for p in pickelized], dtype=int)
-
-    joined = np.frombuffer(b''.join(pickelized), dtype=np.int8)
+    counts_in_l = list()
+    joined_l    = list()
+    for data_in in data_in_l:
+      pickelized = [pickle.dumps(data) for data in data_in]
+      counts_in_l.append(np.array([len(p) for p in pickelized], dtype=int))
+      joined_l.append(np.frombuffer(b''.join(pickelized), dtype=np.int8))
     
-    data_out, counts_out = self.Put_v(joined, counts_in)
+    data_out, counts_out = self.Put_v(joined_l, counts_in_l)
     
     out = []
     r_start = 0
--- maia/utils/ndarray/layouts.pybind.cpp
+++ maia/utils/ndarray/layouts.pybind.cpp
@@ -107,6 +107,49 @@ counting_sort(py::array_t<int64_t>& np_array, int n_bins) {
   return std::make_tuple(np_out, np_counts);
 }
 
+std::tuple<py::list, py::array_t<int64_t>>
+counting_sort_mult(py::list array_list, int n_bins) {
+
+  auto np_counts = py::array_t<int64_t>(n_bins);
+  auto counts = np_counts.mutable_data();
+  for(size_t i=0; i < n_bins; ++i) {
+    counts[i] = 0;
+  }
+
+
+  for (auto item: array_list) {
+    auto np_array = py::cast<py::array_t<int>>(item);
+    size_t size = np_array.size();
+    auto array = np_array.data();
+    for(size_t i=0; i < size; ++i) {
+      counts[array[i]]++;
+    }
+  }
+
+  std::vector<int> displs(n_bins+1, 0);
+  for(size_t i=0; i < n_bins; ++i) {
+    displs[i+1] = displs[i] + counts[i];
+    counts[i] = 0;
+  }
+
+  py::list out_list;
+  
+  for (auto item: array_list) {
+    auto np_array = py::cast<py::array_t<int>>(item);
+    auto array = np_array.data();
+    size_t size = np_array.size();
+    auto np_out = py::array_t<int64_t>(size);
+    auto out = np_out.mutable_data();
+    for(size_t i=0; i < size; ++i) {
+      out[i] = displs[array[i]] + counts[array[i]];
+      counts[array[i]]++;
+    }
+    out_list.append(np_out);
+  }
+
+  return std::make_tuple(out_list, np_counts);
+}
+
 template<typename T>
 py::array_t<T>
 extract_from_indices(py::array_t<T>& np_array, 
@@ -461,6 +504,9 @@ void register_layouts_module(py::module_& parent) {
         py::arg("array2").noconvert());
   m.def("counting_sort", &counting_sort,
         py::arg("array").noconvert(),
+        py::arg("n_bins").noconvert());
+  m.def("counting_sort_mult", &counting_sort_mult,
+        py::arg("arrays").noconvert(),
         py::arg("n_bins").noconvert());
   m.def("take_stridedDI", &take_stridedDI,
         py::arg("counts").noconvert(),
