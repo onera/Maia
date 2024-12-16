b1c85350e801bb412978ac9e5f2403d69163e2fa
Julien Coulet <julien.coulet@onera.fr>
2024-12-16 17:24:28 +0100

    Add opposite func

--- maia/pytree/sids/adjust.py
+++ maia/pytree/sids/adjust.py
@@ -1,3 +1,4 @@
+import warnings
 from maia.pytree.typing import *
 from maia.pytree.meta import api_export
 
@@ -25,13 +26,13 @@ def subregion_fields_to_bcdataset(tree:CGNSTree, mode:str='move'):
 
   The operation performed depends of the value of ``mode`` argument:
 
-  - if ``mode == 'move'``, fields are removed from the ZSR node;
+  - if ``mode == 'move'``, fields are removed from the ZSR node (which is itself preserved);
   - if ``mode == 'copy'``, fields remains in the ZSR node and a copy is done in the BCDataSet;
   - if ``mode == 'view'``, fields in the ZSR and in the BCDataSet share the same memory.
 
   Args:
     tree (CGNSTree): Input tree (starting at root level)
-    mode (str, optional): Controls how the BCDataSet fields are created (see above). Defaults to ``'move'``.
+    mode (str, optional): Controls how the fields are created (see above). Defaults to ``'move'``.
 
   Example:
     >>> tree = PT.yaml.to_cgns_tree('''
@@ -74,3 +75,81 @@ def subregion_fields_to_bcdataset(tree:CGNSTree, mode:str='move'):
         PT.add_child(bcdata_n, copy_or_view(fld_n))
       if mode == 'move':
         PT.rm_children_from_label(zsr_n, 'DataArray_t')
+
+@api_export
+def subregion_fields_from_bcdataset(tree:CGNSTree, mode:str='move'):
+  """ Move the data fields to ZoneSubRegion nodes from their related BC node, if existing.
+  
+  ZoneSubRegion nodes must be explicitly related to a BC node through the BCRegionName descriptor.
+  Data fields are taken from a *full* (without Subset) BCDataSet node, preferentially 
+  named as the ZoneSubRegion node (see :func:`subregion_fields_to_bcdataset`);
+  if such a node does not exists, the last *full* BCDataSet is taken.
+
+  The operation performed depends of the value of ``mode`` argument:
+
+  - if ``mode == 'move'``, fields are removed from the BCDataSet node (which is itself preserved);
+  - if ``mode == 'copy'``, fields remains in the BCDataSet node and a copy is done in the ZSR;
+  - if ``mode == 'view'``, fields in the BCDataSet and in the ZSR share the same memory.
+
+  Args:
+    tree (CGNSTree): Input tree (starting at root level)
+    mode (str, optional): Controls how the fields are created (see above). Defaults to ``'move'``.
+
+  Example:
+    >>> tree = PT.yaml.to_cgns_tree('''
+    ... Zone Zone_t:
+    ...   ZoneBC ZoneBC_t:
+    ...     Wing BC_t 'BCWall':
+    ...       WingExtraction BCDataSet_t "UserDefined":
+    ...         DirichletData BCData_t:
+    ...           field DataArray_t I4 [10, 20, 30, 40]:
+    ...   WingExtraction ZoneSubRegion_t:
+    ...     BCRegionName Descriptor_t "Wing":
+    ... ''')
+    >>> PT.subregion_fields_from_bcdataset(tree)
+    >>> PT.print_tree(PT.get_node_from_label(tree, 'Zone_t'))
+    Zone Zone_t 
+    ├───ZoneBC ZoneBC_t 
+    │   └───Wing BC_t "BCWall"
+    │       └───WingExtraction BCDataSet_t "UserDefined"
+    └───WingExtraction ZoneSubRegion_t 
+        ├───BCRegionName Descriptor_t "Wing"
+        └───field DataArray_t I4 [10 20 30 40]
+  """
+  if mode not in ['move', 'copy', 'view']:
+    raise ValueError(f"Unvalid value for argument mode : {mode}")
+
+  copy_or_view = (lambda n : PT.deep_copy(n)) if mode == 'copy' else (lambda n : n)
+
+  for zone in PT.iter_all_Zone_t(tree):
+    for zsr_n in PT.get_children_from_predicate(zone, IS_RELATED_ZSR):
+
+      zsr_name = PT.get_name(zsr_n)
+      bc_name  = PT.get_value(PT.get_child_from_name(zsr_n, "BCRegionName"))
+
+      bc_n = PT.get_child_from_predicates(zone, f"ZoneBC_t/{bc_name}")
+      
+      is_full_bcds = lambda n : PT.get_label(n) == 'BCDataSet_t' \
+                            and PT.get_child_from_name(n, 'PointList') is None \
+                            and PT.get_child_from_name(n, 'PointRange') is None
+      bc_ds_list = PT.get_children_from_predicate(bc_n, is_full_bcds)
+      if len(bc_ds_list) > 0:
+        # Select relevant BCDS : take the one having ZSR name in priority. Otherwise, take last one
+        for bc_ds in bc_ds_list:
+          if PT.get_name(bc_ds) == zsr_name:
+            break
+
+        for fld_n in PT.get_children_from_predicates(bc_ds, "BCData_t/DataArray_t"):
+          PT.rm_children_from_name(zsr_n, PT.get_name(fld_n))
+          if PT.get_value(fld_n).size == 1 and (bc_size:=PT.Subset.n_elem(bc_n)) != 1: # Auto extend scalar data
+            if mode == 'view':
+              msg = f"On ZoneSubRegion '{zsr_name}', can not create of view of scalar data '{fld_n[0]}'" \
+                    f" from BCDataSet '{bc_ds[0]}', a copy is done instead"
+              warnings.warn(msg, stacklevel=2)
+            fld_n = PT.shallow_copy(fld_n)
+            fld_val = PT.get_value(fld_n)
+            PT.set_value(fld_n, fld_val*np.ones(bc_size, fld_val.dtype))
+
+          PT.add_child(zsr_n, copy_or_view(fld_n))
+        if mode == 'move':
+          PT.rm_children_from_label(bc_ds, 'BCData_t')
--- maia/pytree/sids/test/test_adjust.py
+++ maia/pytree/sids/test/test_adjust.py
@@ -43,3 +43,39 @@ def test_zsr_fields_to_bc(mode):
   if mode == 'move':
     assert PT.get_node_from_path(tree, 'Base/Zone/LinkedZSR/field') is None
   assert PT.get_node_from_path(tree, 'Base/Zone/LinkedZSR') is not None
+
+@pytest.mark.parametrize('mode', ['move', 'view'])
+def test_zsr_fields_from_bc(mode):
+  yt = """
+  Base CGNSBase_t:
+    Zone Zone_t:
+      ZoneBC ZoneBC_t:
+        BC1 BC_t:
+        BC2 BC_t:
+          PointList IndexArray_t [[2,4,6,8]]:
+          GridLocation GridLocation_t "Vertex":
+          LinkedZSR BCDataSet_t:
+            DirichletData BCData_t:
+              LocalData DataArray_t [10,20,30,40]:
+              GlobalData DataArray_t [18.]:
+          RandomBCDS BCDataSet_t:
+            DirichletData BCData_t:
+              RandomData DataArray_t [42.]:
+      UnlinkedZSR ZoneSubRegion_t:
+        field DataArray_t [1,2,3,4]:
+      LinkedZSR ZoneSubRegion_t:
+        BCRegionName Descriptor_t "BC2":
+  """
+  tree = PT.yaml.parse_yaml_cgns.to_cgns_tree(yt)
+
+  if mode == 'view':
+    with pytest.warns():
+      adjust.subregion_fields_from_bcdataset(tree, mode)
+  else:
+    adjust.subregion_fields_from_bcdataset(tree, mode)
+
+  assert (PT.get_node_from_path(tree, 'Base/Zone/LinkedZSR/GlobalData')[1] == [18., 18, 18, 18]).all()
+  assert (PT.get_node_from_path(tree, 'Base/Zone/LinkedZSR/LocalData')[1] == [10, 20, 30, 40]).all()
+  if mode == 'move':
+    assert PT.get_node_from_path(tree, 'Base/Zone/ZoneBC/BC2/LinkedZSR/DirichletData') is None
+  assert PT.get_node_from_path(tree, 'Base/Zone/ZoneBC/BC2/RandomBCDS') is not None
