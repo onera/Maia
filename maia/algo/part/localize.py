f782f62a35cfb0a8522310cff004e3e292151962
Julien Coulet <julien.coulet@onera.fr>
2025-02-12 10:03:19 +0100

    Minor refactor of localize to facilitate setup of custom point cloud

--- maia/algo/part/localize.py
+++ maia/algo/part/localize.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mpi4py import MPI
 
 import Pypdm.Pypdm as PDM
 
@@ -10,7 +11,7 @@ from maia.utils                  import py_utils, np_utils, par_utils
 from maia.transfer               import utils as te_utils
 from maia.factory.dist_from_part import get_parts_per_blocks
 
-from .point_cloud_utils  import get_shifted_point_clouds
+from .point_cloud_utils  import get_point_cloud
 from .connectivity_utils import cell_vtx_connectivity_elts
 
 def _get_part_data_ngon(part_zone):
@@ -71,7 +72,7 @@ def _mesh_location(src_parts, tgt_clouds, comm, reverse=False, loc_tolerance=1E-
   """ Wrapper of PDM mesh location
   For now, only 1 domain is supported so we expect source parts and target clouds
   as flat lists :
-  Parts are tuple dim, elt_kind, part_data 
+  Parts are tuple (dim, elt_kind, part_data)
    where dim = 2 or 3, elt_kind = 'Poly' or 'Element' and part_data stores the arrays
    expected by paradigm
   Cloud are tuple (coords, lngn)
@@ -120,26 +121,85 @@ def _mesh_location(src_parts, tgt_clouds, comm, reverse=False, loc_tolerance=1E-
   else:
     return all_target_data
 
-def _localize_points(src_parts_per_dom, tgt_parts_per_dom, location, comm, \
-    reverse=False, loc_tolerance=1E-6):
+def _mdom_mesh_location(src_parts_per_dom, tgt_clouds_per_dom, comm, reverse=False, loc_tolerance=1E-6):
   """
+  Wraps _mesh_location with multidomain support (with shifts)
+  Input are similar to _mesh_location, but with nested lists by domains
+  Input data must not be shifted, it will be done by this function
   """
-  locs = {'NGon2D' :{'Cell':2, 'Vtx':5},
-          'NGon3D' :{'Cell':2, 'Face':5, 'Vtx':7},
-          'Element':{'Cell':2, 'Vtx':4}}
-  n_dom_src = len(src_parts_per_dom)
-  n_dom_tgt = len(tgt_parts_per_dom)
+  
+  n_part_per_dom_src = [len(parts) for parts in src_parts_per_dom ]
+  n_part_per_dom_tgt = [len(parts) for parts in tgt_clouds_per_dom]
 
-  n_part_per_dom_src = [len(parts) for parts in src_parts_per_dom]
-  n_part_per_dom_tgt = [len(parts) for parts in tgt_parts_per_dom]
-  n_part_src = sum(n_part_per_dom_src)
-  n_part_tgt = sum(n_part_per_dom_tgt)
+  # Shift target data first; we dont do it inplace since src and target data
+  # may share the same memory
+  tgt_offset = np.zeros(len(tgt_clouds_per_dom)+1, dtype=pdm_gnum_dtype)
+  for i_domain, clouds in enumerate(tgt_clouds_per_dom):
+    # Compute global offsets for this domain
+    dom_max = par_utils.arrays_max([cloud[1] for cloud in clouds], comm)
+    tgt_offset[i_domain+1] = tgt_offset[i_domain] + dom_max
+    # Shift source arrays (copy)
+    tgt_clouds_per_dom[i_domain] = [(c[0], c[1] + tgt_offset[i_domain]) for c in clouds]
 
-  # > Register source
+  tgt_clouds = py_utils.to_flat_list(tgt_clouds_per_dom)
+  src_parts  = py_utils.to_flat_list(src_parts_per_dom)
+
+  # Now shift source data. First we need to eetrieve loc, which should be the same for each partition.
+  kind = ''
+  if len(src_parts) > 0:
+    dim, kind = src_parts[0][:2]
+    if kind == 'Poly':
+      kind += str(dim)
+  kind = comm.allreduce(kind, MPI.MAX) # To let empty procs know the data
+
+  locs = {'Poly2'   : {'Cell':2, 'Vtx':5},           
+          'Poly3'   : {'Cell':2, 'Face':5, 'Vtx':7},
+          'Element' : {'Cell':2, 'Vtx':4},         
+         }[kind]
+
+  # Effective shift
+  src_offsets = {loc : np.zeros(len(src_parts_per_dom)+1, dtype=pdm_gnum_dtype) for loc in locs}
+  for i_domain, src_parts_domain in enumerate(src_parts_per_dom):
+    # Compute global offsets for this domain
+    for loc, array_idx in locs.items():
+      dom_max = par_utils.arrays_max([src_part[2][array_idx] for src_part in src_parts_domain], comm)
+      src_offsets[loc][i_domain+1] = src_offsets[loc][i_domain] + dom_max
+    # Shift source arrays (inplace)
+    for src_part in src_parts_domain:
+      for loc, array_idx in locs.items():
+        src_part[2][array_idx] += src_offsets[loc][i_domain]
+
+  result = _mesh_location(src_parts, tgt_clouds, comm, reverse, loc_tolerance)
+
+  # Shift back source data
+  for i_domain, src_parts_domain in enumerate(src_parts_per_dom):
+    for src_part in src_parts_domain:
+      for loc, array_idx in locs.items():
+        src_part[2][array_idx] -= src_offsets[loc][i_domain]
+
+  # Shift results and get domain ids
+  direct_result = result[0] if reverse else result
+  for tgt_result in direct_result:
+    tgt_result['location_shifted'] = tgt_result.pop('location') #Rename key
+    tgt_result['location'], tgt_result['domain'] = np_utils.shifted_to_local(
+        tgt_result['location_shifted'], src_offsets['Cell'])
+  if reverse:
+    for src_result in result[1]:
+      src_result['points_gnum_shifted'] = src_result.pop('points_gnum') #Rename key
+      src_result['points_gnum'], src_result['domain'] = np_utils.shifted_to_local(
+          src_result['points_gnum_shifted'], tgt_offset)
+
+  # Reshape output to list of lists (as input domains)
+  if reverse:
+    return py_utils.to_nested_list(result[0], n_part_per_dom_tgt),\
+           py_utils.to_nested_list(result[1], n_part_per_dom_src)
+  else:
+    return py_utils.to_nested_list(result, n_part_per_dom_tgt)
+
+def _collect_source(src_parts_per_dom):
   connectivity_t = None
   src_parts = []
-  for i_domain, src_part_zones in enumerate(src_parts_per_dom):
-    # TODO : use cell_vtx_connectivity_ngon to transform ngon into nodal ?
+  for src_part_zones in src_parts_per_dom:
 
     src_parts_domain = list()
     for src_part in src_part_zones:
@@ -156,49 +216,25 @@ def _localize_points(src_parts_per_dom, tgt_parts_per_dom, location, comm, \
         src_parts_domain.append((dim, 'Element', _get_part_data_elts(src_part)))
     src_parts.append(src_parts_domain)
 
-  locs = locs[connectivity_t]
-  src_offsets = {loc : np.zeros(n_dom_src+1, dtype=pdm_gnum_dtype) for loc in locs}
-  for i_domain, src_parts_domain in enumerate(src_parts):
-    # Compute global offsets for this domain
-    for loc, array_idx in locs.items():
-      dom_max = par_utils.arrays_max([src_part[2][array_idx] for src_part in src_parts_domain], comm)
-      src_offsets[loc][i_domain+1] = src_offsets[loc][i_domain] + dom_max
-    # Shift source arrays (inplace)
-    for src_part in src_parts_domain:
-      for loc, array_idx in locs.items():
-        src_part[2][array_idx] += src_offsets[loc][i_domain]
-  
-  src_parts = py_utils.to_flat_list(src_parts)
+  return src_parts
 
-  tgt_offset, tgt_clouds = get_shifted_point_clouds(tgt_parts_per_dom, location, comm)
-  tgt_clouds = py_utils.to_flat_list(tgt_clouds)
+def _collect_target(tgt_parts_per_dom, location):
+  return [[get_point_cloud(part, location) for part in tgt_parts] \
+          for tgt_parts in tgt_parts_per_dom]
 
-  result = _mesh_location(src_parts, tgt_clouds, comm, reverse, loc_tolerance)
 
-  # Shift back source data
-  for i_domain, src_parts_domain in enumerate(py_utils.to_nested_list(src_parts, n_part_per_dom_src)):
-    for src_part in src_parts_domain:
-      for loc, array_idx in locs.items():
-        src_part[2][array_idx] -= src_offsets[loc][i_domain]
+def _localize_points(src_parts_per_dom, tgt_parts_per_dom, location, comm, \
+    reverse=False, loc_tolerance=1E-6):
+  """ Intermediate API who do not place output in tree.
+  Inputs are list of size n_domain_src (resp. tgt) containing partitioned zones (resp. clouds)
+  for each domain 
+  """
+  src_parts  = _collect_source(src_parts_per_dom)
+  tgt_clouds = _collect_target(tgt_parts_per_dom, location)
 
-  # Shift results and get domain ids
-  direct_result = result[0] if reverse else result
-  for tgt_result in direct_result:
-    tgt_result['location_shifted'] = tgt_result.pop('location') #Rename key
-    tgt_result['location'], tgt_result['domain'] = np_utils.shifted_to_local(
-        tgt_result['location_shifted'], src_offsets['Cell'])
-  if reverse:
-    for src_result in result[1]:
-      src_result['points_gnum_shifted'] = src_result.pop('points_gnum') #Rename key
-      src_result['points_gnum'], src_result['domain'] = np_utils.shifted_to_local(
-          src_result['points_gnum_shifted'], tgt_offset)
-  
-  # Reshape output to list of lists (as input domains)
-  if reverse:
-    return py_utils.to_nested_list(result[0], n_part_per_dom_tgt),\
-           py_utils.to_nested_list(result[1], n_part_per_dom_src)
-  else:
-    return py_utils.to_nested_list(result, n_part_per_dom_tgt)
+  return _mdom_mesh_location(src_parts, tgt_clouds, comm, reverse, loc_tolerance)
+
+
 
 def localize_points(src_tree, tgt_tree, location, comm, **options):
   """Localize points between two partitioned trees.
