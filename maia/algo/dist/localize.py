c85476382b229d59fc9b40090ded9862c14a6c35
Julien Coulet <julien.coulet@onera.fr>
2025-02-13 11:24:20 +0100

    Cleanup + add tests + simplify _localize_points

--- maia/algo/dist/localize.py
+++ maia/algo/dist/localize.py
@@ -1,23 +1,20 @@
 from mpi4py import MPI
 import numpy as np
 
-import Pypdm.Pypdm as PDM
-
 import maia.pytree        as PT
 import maia.pytree.maia   as MT
 
-import maia
-from maia                        import npy_pdm_gnum_dtype as pdm_gnum_dtype
-from maia.utils                  import py_utils, np_utils, par_utils
-from maia.transfer               import utils as te_utils
-from maia.factory.dist_from_part import get_parts_per_blocks
+from maia          import npy_pdm_gnum_dtype as pdm_gnum_dtype
+from maia.utils    import py_utils, np_utils, par_utils
 
 from maia.transfer import protocols as EP
 
-#from .point_cloud_utils  import get_shifted_point_clouds
-#from .connectivity_utils import cell_vtx_connectivity_elts
+from maia.algo.indexing                import pe_to_nface, ngon_to_edge_pe, get_pe_local
+from maia.algo.dist.geometry           import _compute_elements_center
+from maia.algo.dist.ngon_tools         import PDM_dfacecell_to_dcellface
+from maia.algo.dist.connectivity_utils import entity_vtx_connectivity_elt, cell_vtx_connectivity_S
 
-from maia.algo.geometry  import _compute_elements_center
+from maia.algo.part.localize import _mdom_mesh_location as _mdom_mesh_location_part
 
 is_poly_3d_zone = lambda z: PT.Zone.CellDimension(z) == 3 and PT.Zone.has_ngon_elements(z)
 is_poly_2d_zone = lambda z: PT.Zone.CellDimension(z) == 2 and \
@@ -45,7 +42,7 @@ def minimal_partitioning(zone, comm, use_geom=False):
     cell_gnum = np.arange(cell_distri[0]+1, cell_distri[1]+1, dtype=pdm_gnum_dtype)
 
   if is_poly_3d_zone(zone):
-    maia.algo.pe_to_nface(zone, comm) # Create NFace if not already existing
+    pe_to_nface(zone, comm) # Create NFace if not already existing
     ngon  = PT.Zone.NGonNode(zone)
     nface = PT.Zone.NFaceNode(zone)
 
@@ -81,11 +78,10 @@ def minimal_partitioning(zone, comm, use_geom=False):
 
     edge  = MT.Zone.EdgeNode(zone)
     if PT.get_child_from_name(edge, 'ParentElements') is None:
-      maia.algo.ngon_to_edge_pe(zone, comm)
+      ngon_to_edge_pe(zone, comm)
 
     # Prepare dface_edge on distributed input
-    from maia.algo.dist.ngon_tools import PDM_dfacecell_to_dcellface
-    local_pe = maia.algo.indexing.get_pe_local(edge).reshape(-1, order='C')
+    local_pe = get_pe_local(edge).reshape(-1, order='C')
 
     _edge_distri = par_utils.partial_to_full_distribution(MT.get_distribution(edge, 'Element')[1], comm)
     _face_distri = par_utils.partial_to_full_distribution(cell_distri, comm)
@@ -114,7 +110,6 @@ def minimal_partitioning(zone, comm, use_geom=False):
   else:
     # For elt and S zones: cell_vtx ('volumic' cells only), coords, cell_gnum, vtx_gnum
 
-    from maia.algo.dist.connectivity_utils import entity_vtx_connectivity_elt, cell_vtx_connectivity_S
     if PT.Zone.Type(zone) == 'Structured':
       dcell_vtx_idx, dcell_vtx = cell_vtx_connectivity_S(zone, dim)
     else:
@@ -145,14 +140,7 @@ def minimal_partitioning(zone, comm, use_geom=False):
   return part_data
 
 
-
-   
-
-
-
-
-# TODO : factorize ?
-def get_point_cloud(zone, comm, location='CellCenter'):
+def get_point_cloud(zone, comm, location):
   """
   If location == Vertex, return the (interlaced) coordinates of vertices 
   and vertex global numbering of a partitioned zone
@@ -189,198 +177,65 @@ def get_point_cloud(zone, comm, location='CellCenter'):
 
   raise RuntimeError("Unknow location or node")
 
-# TODO : factorize ? 
-def get_shifted_point_clouds(dom_list, location, comm):
-  """ Wraps get_point_cloud around multiple domains,
-  shifting lngn with previous values"""
-  coords_per_dom = []
-  lngn_per_dom = []
-  clouds = []
-  offset = np.zeros(len(dom_list)+1, dtype=pdm_gnum_dtype)
-  for idom, zone in enumerate(dom_list):
-    coords, lngn = get_point_cloud(zone, comm, location)
-    coords_per_dom.append(coords)
-    lngn_per_dom.append(lngn_per_dom)
-    max = par_utils.arrays_max([lngn], comm)
-    offset[idom+1] = offset[idom] + max
-    lngn += offset[idom]
 
-    clouds.append((coords, lngn))
+def _mdom_mesh_location(src_parts, tgt_clouds, comm, reverse=False, loc_tolerance=1E-6):
 
-  return offset, clouds
-
-
-
-
-
-def _get_part_data_ngon(zone, comm):
-  dim = PT.Zone.CellDimension(zone)
+  # Add a level in list to mimic partitions
+  tgt_clouds_per_dom = [[c] for c in tgt_clouds]
+  src_parts_per_dom  = [[p] for p in src_parts]
   
-  pdata = minimal_partitioning(zone, comm)
+  result = _mdom_mesh_location_part(src_parts_per_dom, tgt_clouds_per_dom, comm, reverse, loc_tolerance)
 
-  if dim == 3:
-    
-    pcell_face_idx, pcell_face, pface_vtx_idx, pface_vtx, pcoords, \
-        cell_gnum, face_gnum, vtx_gnum = pdata
-
-    return [pcell_face_idx, pcell_face, cell_gnum, \
-        pface_vtx_idx, pface_vtx, face_gnum, pcoords, vtx_gnum]
-
-  elif dim == 2:
-    
-    pface_edge_idx, pface_edge, pedge_vtx, pcoords, \
-        cell_gnum, vtx_gnum = pdata
-  
-    return [pface_edge_idx, pface_edge, cell_gnum, pedge_vtx, pcoords, vtx_gnum]
-
-
-def _get_part_data_elts(zone, comm):
-  pcell_vtx_idx, pcell_vtx, pvtx_coords, cell_gnum, vtx_gnum = minimal_partitioning(zone, comm)
-
-  return [pcell_vtx_idx, pcell_vtx, cell_gnum, pvtx_coords, vtx_gnum]
-    
-
-def _mesh_location(src_parts, tgt_clouds, comm, reverse=False, loc_tolerance=1E-6):
-  """ Wrapper of PDM mesh location
-  For now, only 1 domain is supported so we expect source parts and target clouds
-  as flat lists :
-  Parts are tuple dim, elt_kind, part_data 
-   where dim = 2 or 3, elt_kind = 'Poly' or 'Element' and part_data stores the arrays
-   expected by paradigm
-  Cloud are tuple (coords, lngn)
-  """
-
-  n_part_src = len(src_parts)
-  n_part_tgt = len(tgt_clouds)
-  # > Create and setup global data
-  mesh_loc = PDM.MeshLocation(n_point_cloud=1, comm=comm)
-  mesh_loc.mesh_n_part_set(n_part_src)
-  mesh_loc.n_part_cloud_set(0, n_part_tgt)
-
-  # > Register source
-  for i_part, part_data in enumerate(src_parts):
-    dim, kind, _part_data = part_data
-    assert dim >= 2, "Dimension lower than 2 are not supported"
-    set_func = {'Poly' :    [mesh_loc.part_set_2d, mesh_loc.part_set],
-                'Element' : [mesh_loc.nodal_part_set_2d, mesh_loc.nodal_part_set]}[kind][dim-2]
-    set_func(i_part, *_part_data)
-
-
-  # > Setup target
-  for i_part, (coords, lngn) in enumerate(tgt_clouds):
-    mesh_loc.cloud_set(0, i_part, coords, lngn)
-
-  mesh_loc.tolerance = loc_tolerance
-  mesh_loc.compute()
-
-  # This is located and unlocated indices
-  all_located_id   = [mesh_loc.located_get  (0,i_part) for i_part in range(n_part_tgt)]
-  all_unlocated_id = [mesh_loc.unlocated_get(0,i_part) for i_part in range(n_part_tgt)]
-
-  #This is result from the target perspective (api : (i_pt_cloud, i_part))
-  all_target_data = [mesh_loc.location_get(0, i_tgt_part) for i_tgt_part in range(n_part_tgt)]
-  # Add ids in dict
-  for i_part, data in enumerate(all_target_data):
-    data.pop('g_num')
-    data['located_ids']   = all_located_id[i_part] - 1
-    data['unlocated_ids'] = all_unlocated_id[i_part] - 1
-
-  #This is result from the source perspective (api : ((i_pt_cloud, i_part))
+  # Remove intermediate level
   if reverse:
-    all_located_inv = [{**mesh_loc.points_in_elt_get(0, i_src_part), **mesh_loc.cell_vertex_get(i_src_part)} \
-                       for i_src_part in range(n_part_src)]
-    return all_target_data, all_located_inv
+    return py_utils.to_flat_list(result[0]), py_utils.to_flat_list(result[1])
   else:
-    return all_target_data
-
-
-def _localize_points(src_dom, tgt_dom, location, comm, \
-    reverse=False, loc_tolerance=1E-6):
-  """
-  """
-  locs = {'NGon2D' :{'Cell':2, 'Vtx':5},
-          'NGon3D' :{'Cell':2, 'Face':5, 'Vtx':7},
-          'Element':{'Cell':2, 'Vtx':4}}
-  n_dom_src = len(src_dom)
-
-
+    return py_utils.to_flat_list(result)
+    
+def _collect_source(src_doms, comm):
   # > Register source
   connectivity_t = None
   src_parts = []
-  for i_domain, dom in enumerate(src_dom):
 
-    dim = PT.Zone.CellDimension(dom)
-    if PT.Zone.has_ngon_elements(dom):
+  for zone in src_doms:
+
+    dim = PT.Zone.CellDimension(zone)
+    if PT.Zone.has_ngon_elements(zone):
       if connectivity_t=='Element':
         raise NotImplementedError("Source mesh must have NGon or Element connectivity but not both.")
-      connectivity_t = f'NGon{dim}D'
-      src_parts.append((dim, 'Poly', _get_part_data_ngon(dom, comm)))
+      connectivity_t = 'Poly'
+      selector = [0,1,5,2,3,6,4,7] if dim == 3 else [0,1,4,2,3,5]
     else:
-      if connectivity_t is not None and 'NGON' in connectivity_t:
+      if connectivity_t == 'Poly':
         raise NotImplementedError("Source mesh must have NGon or Element connectivity but not both.")
       connectivity_t = 'Element'
-      src_parts.append((dim, 'Element', _get_part_data_elts(dom, comm)))
+      selector = [0,1,3,2,4]
 
-  locs = locs[connectivity_t]
-  src_offsets = {loc : np.zeros(n_dom_src+1, dtype=pdm_gnum_dtype) for loc in locs}
-  for i_domain, src_part in enumerate(src_parts):
-    # Compute global offsets for this domain
-    for loc, array_idx in locs.items():
-      dom_max = par_utils.arrays_max([src_part[2][array_idx]], comm)
-      src_offsets[loc][i_domain+1] = src_offsets[loc][i_domain] + dom_max
-    # Shift source arrays (inplace)
-    for loc, array_idx in locs.items():
-      src_part[2][array_idx] += src_offsets[loc][i_domain]
+    _part_data = minimal_partitioning(zone, comm, use_geom=False) # mesh_loc apply hilbert itself
+    part_data = [_part_data[i] for i in selector] # Put in order expected by mesh_location
+    src_parts.append((dim, connectivity_t, part_data))
+
+  return src_parts
+
+def _collect_target(tgt_doms, location, comm):
+  """ Wraps get_point_cloud around multiple domains """
+  return [get_point_cloud(zone, comm, location) for zone in tgt_doms]
+
+def _localize_points(src_dom, tgt_dom, location, comm, \
+    reverse=False, loc_tolerance=1E-6):
+  """ Intermediate API who do not place output in tree.
+  Inputs are list of size n_domain_src (resp. tgt) containing distributed zones (resp. clouds)
+  """
+  src_parts  = _collect_source(src_dom, comm)
+  tgt_clouds = _collect_target(tgt_dom, location, comm)
+
+  return _mdom_mesh_location(src_parts, tgt_clouds, comm, reverse, loc_tolerance)
 
 
-  tgt_offset, tgt_clouds = get_shifted_point_clouds(tgt_dom, location, comm)
-  
-
-  result = _mesh_location(src_parts, tgt_clouds, comm, reverse, loc_tolerance)
-
-  # No need to shift back source data, because it was only a copy
-
-  # Shift results and get domain ids
-  direct_result = result[0] if reverse else result
-  for tgt_result in direct_result:
-    tgt_result['location_shifted'] = tgt_result.pop('location') #Rename key
-    tgt_result['location'], tgt_result['domain'] = np_utils.shifted_to_local(
-        tgt_result['location_shifted'], src_offsets['Cell'])
-  if reverse:
-    for src_result in result[1]:
-      src_result['points_gnum_shifted'] = src_result.pop('points_gnum') #Rename key
-      src_result['points_gnum'], src_result['domain'] = np_utils.shifted_to_local(
-          src_result['points_gnum_shifted'], tgt_offset)
-
-  # Reshape output to list of lists (as input domains)
-  return result
 
 def localize_points(src_tree, tgt_tree, location, comm, **options):
-  """Localize points between two distributed trees.
-
-  For all the points of the target tree matching the given location,
-  search the cell of the source tree in which it is enclosed.
-  The result, i.e. the gnum & domain number of the source cell (or -1 if the point is not localized),
-  are stored in a ``DiscreteData_t`` container called "Localization" on the target zones.
-
-  Source tree must be unstructured.
-
-  Localization can be parametred thought the options kwargs:
-
-  - ``loc_tolerance`` (default = 1E-6) -- Geometric tolerance for the method.
-
-  Args:
-    src_tree (CGNSTree): Source tree, partitionned. Only unstructured connectivities are managed.
-    tgt_tree (CGNSTree): Target tree, partitionned.
-    location ({'CellCenter', 'Vertex'}) : Target points to localize
-    comm       (MPIComm): MPI communicator
-    **options: Additional options related to location strategy
-
-  Example:
-      .. literalinclude:: snippets/test_algo.py
-        :start-after: #localize_points@start
-        :end-before: #localize_points@end
-        :dedent: 2
+  """
+  Distributed counterpart of maia.algo.part.localize_points
   """
   src_path = PT.predicates_to_paths(src_tree, 'CGNSBase_t/Zone_t')
   src_dom = [PT.get_node_from_path(src_tree, path) for path in src_path]
