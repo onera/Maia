df31174483c686d1dfb9effa544da876a0907c4a
Julien Coulet <julien.coulet@onera.fr>
2025-03-28 12:00:17 +0100

    Reimplement func in python

--- maia/algo/dist/fsdm_distribution/__init__.py
+++ maia/algo/dist/fsdm_distribution/__init__.py
@@ -1,9 +1,21 @@
-from cmaia import dist_algo as cdist_algo
-from maia.algo.apply_function_to_nodes import apply_to_bases,apply_to_zones
+import maia.pytree      as PT
+import maia.pytree.maia as MT
 
-from maia.utils import require_cpp20
+from maia.utils import par_utils
 
-@require_cpp20
 def add_fsdm_distribution(t, comm):
-  apply_to_bases(cdist_algo.add_fsdm_distribution, t, comm)
+  for base in PT.get_all_CGNSBase_t(t):
+    zones = PT.get_all_Zone_t(base)
+    if len(zones) != 1:
+      raise RuntimeError("add_fsdm_distribution (as FSDM) expects only one zone per process")
+    zone = zones[0]
 
+    n_vtx_owned = PT.get_node_from_path(zone, ':CGNS#LocalNumbering/VertexSizeOwned')[1][0]
+    vtx_distri = par_utils.dn_to_distribution(n_vtx_owned, comm)
+    MT.new_distribution({"Vertex" : vtx_distri}, zone)
+
+    for elt_section in PT.get_children_from_label(zone, 'Elements_t'):
+      n_owned_elt = PT.Element.Size(elt_section)
+      elt_distri = par_utils.dn_to_distribution(n_owned_elt, comm)
+      MT.new_distribution({"Element" : elt_distri}, elt_section)
+    
