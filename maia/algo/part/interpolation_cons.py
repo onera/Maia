ef74026db9d1165d701a88351d5a4dd40d7f4ea0
Julien Coulet <julien.coulet@onera.fr>
2026-04-02 10:41:45 +0200

    Add is_conservative switch to vtx<->cell interpolators

--- maia/algo/part/interpolation_cons.py
+++ maia/algo/part/interpolation_cons.py
@@ -83,57 +83,62 @@ def tree_dim(tree:CGNSPartTree, comm:MPIComm) -> int:
   return dims.pop()
 
 class VertexToCell:
+
   def __init__(self, tree:CGNSPartTree, comm:MPIComm):
-    self.zones = PT.get_all_Zone_t(tree)
+    zones = PT.get_all_Zone_t(tree)
 
     self.cell_vtx_l = []
-    for zone in self.zones:
+    for zone in zones:
       dim = PT.Zone.CellDimension(zone)
       elt = PT.find_child_from_predicate(zone, PT.pred.is_element_of_type('TRI_3' if dim == 2 else 'TETRA_4'))
       self.cell_vtx_l.append(MT.Element.connectivity(elt))
 
-    self.dim = dim if len(self.zones) > 0 else -1
+    dim = dim if len(zones) > 0 else 0
 
     # Weight must be already extended (size = cell_vtx.dsize)
+
     # Method 1: Basic arithmetic mean
     #   Within a cell, each vertex contributes with a constant weight
     #   w = 1 / n_vtx_of_cell  
-    self.weight_l = [1./(self.dim+1) * len(self.zones)] # Trick : use cste, numpy will broadcast
+    #   ---> Suitable for conservative fields (eg density)
+    self.cons_weight_l = [1./(dim+1)] * len(zones) # Trick : use cste, numpy will broadcast
 
     # Method 2: Dual volumes
     #  Within a cell, each vertex contributes up to the fraction of dual volume provided by this cell:
     #  w = dual_volume_contribution_from_cell / dual_volume_of_vertex
-    """
-    self.weight_l = []
-    dual_vol_l = PDM.part_mesh_nodal_dual_volume(part_zones_to_pdm_pmesh_nodal(self.zones, comm))
-    for i, zone in enumerate(self.zones):
+    #  ---> Suitable for integrated fields (eg mass)
+    self.inte_weight_l = []
+    dual_vol_l = PDM.part_mesh_nodal_dual_volume(part_zones_to_pdm_pmesh_nodal(zones, comm))
+    for i, zone in enumerate(zones):
       vol = _get_native_measure(zone)
-      vol_dispatch = np.repeat(vol / (self.dim+1), self.dim+1)
+      vol_dispatch = np.repeat(vol / (dim+1), dim+1)
       dual_vol = dual_vol_l[i]
-      self.weight_l.append(vol_dispatch / dual_vol[self.cell_vtx_l[i].values-1])
-    """
+      self.inte_weight_l.append(vol_dispatch / dual_vol[self.cell_vtx_l[i].values-1])
 
-  def _exchange_fields(self, vtx_fields:Dict[str, List[NDArray]]) -> Dict[str, List[NDArray]]:
+  def _exchange_fields(self, vtx_fields:Dict[str, List[NDArray]], is_conservative:bool) -> Dict[str, List[NDArray]]:
 
+    weight_l = self.cons_weight_l if is_conservative else self.inte_weight_l
     cell_fields = {key: [] for key in vtx_fields}
+
     for i, cell_vtx in enumerate(self.cell_vtx_l):
       for fname, vtx_vals_l in vtx_fields.items():
-        rep_field = vs.from_displs(cell_vtx.displs, vtx_vals_l[i][cell_vtx.values-1]*self.weight_l[i])
+        rep_field = vs.from_displs(cell_vtx.displs, vtx_vals_l[i][cell_vtx.values-1]*weight_l[i])
         cell_field = rep_field.reduce(vs.ReduceOp.SUM)
         cell_fields[fname].append(cell_field)
 
     return cell_fields
 
 class CellToVertex:
-  def __init__(self, tree:CGNSPartTree, comm:MPIComm):
-    self.zones = PT.get_all_Zone_t(tree)
 
-    nb_tot_vertex = MT.Zone.n_vtx(self.zones, comm)
+  def __init__(self, tree:CGNSPartTree, comm:MPIComm):
+    zones = PT.get_all_Zone_t(tree)
+
+    nb_tot_vertex = MT.Zone.n_vtx(zones, comm)
     vtx_distri  = par_utils.uniform_distribution(nb_tot_vertex,  comm)
 
     cell_vtx_gnum_l = list()
     vtx_gnum_l = list()
-    for zone in self.zones:
+    for zone in zones:
       dim = PT.Zone.CellDimension(zone)
       elt = PT.find_child_from_predicate(zone, PT.pred.is_element_of_type('TRI_3' if dim == 2 else 'TETRA_4'))
       elt_vtx = PT.get_np_value(PT.find_child_from_name(elt, 'ElementConnectivity'))
@@ -142,7 +147,7 @@ class CellToVertex:
       cell_vtx_gnum_l.append(vtx_gnum[elt_vtx - 1])
       vtx_gnum_l.append(vtx_gnum)
   
-    self.dim = dim if len(self.zones) > 0 else -1
+    self.dim = dim if len(zones) > 0 else 0
 
     # First indexer for cell_vtx connectivity, second one only for vertices
     self.cnt_gi = EP.GlobalIndexer(vtx_distri, cell_vtx_gnum_l, comm, gnum_offset=1)
@@ -153,25 +158,26 @@ class CellToVertex:
     # Method 1: Basic arithmetic mean
     #   From each cell, use constant weight for every vertices
     #   w = 1 / n_vtx_of_cell  
-    """
-    self.weight_l = [1./(self.dim+1) * len(self.zones)] # Trick : use cste, numpy will broadcast
-    """
+    #  ---> Suitable for integrated fields (eg mass)
+    self.inte_weight_l = [1./(self.dim+1)] * len(zones) # Trick : use cste, numpy will broadcast
 
     # Method 2: Dual volumes
     #  From each cell, contribute to each vtx using to the fraction of vtx dual volume provided by the cell:
     #  w = dual_volume_contribution_from_cell / dual_volume_of_vertex
-    volume_l = [_get_native_measure(zone) for zone in self.zones]
+    #   ---> Suitable for conservative fields (eg density)
+    volume_l = [_get_native_measure(zone) for zone in zones]
     vol_dispatch_l = [np.repeat(vol / (self.dim+1), self.dim+1) for vol in volume_l]
     # We already have indexer so compute dual volume manually
     dual_vol_rep_l = self.cnt_gi.Take(self.cnt_gi.Put(vol_dispatch_l, reduce=EP.ReduceOp.SUM))
-    self.weight_l = [vol_dispatch / dual_vol_rep for vol_dispatch, dual_vol_rep in zip(vol_dispatch_l, dual_vol_rep_l)]
+    self.cons_weight_l = [vol_dispatch / dual_vol_rep for vol_dispatch, dual_vol_rep in zip(vol_dispatch_l, dual_vol_rep_l)]
 
-  def _exchange_fields(self, cell_fields:Dict[str, List[NDArray]]) -> Dict[str, List[NDArray]]:
+  def _exchange_fields(self, cell_fields:Dict[str, List[NDArray]], is_conservative:bool) -> Dict[str, List[NDArray]]:
 
+    weight_l = self.cons_weight_l if is_conservative else self.inte_weight_l
     vtx_fields = dict()
 
     for fname, vals in cell_fields.items():
-      data = [np.repeat(val, self.dim+1)*weight for val,weight in zip(vals, self.weight_l)]
+      data = [np.repeat(val, self.dim+1)*weight for val,weight in zip(vals, weight_l)]
       vtx_fields[fname] = self.vtx_gi.Take(self.cnt_gi.Put(data, reduce=EP.ReduceOp.SUM))
 
     return vtx_fields
