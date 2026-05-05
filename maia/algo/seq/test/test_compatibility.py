a091bd7a14bd37d04fc71363dbe30c0824441959
Julien Coulet <julien.coulet@onera.fr>
2026-05-05 15:51:02 +0200

    Enable test_compatibility in CXX17 prods

--- maia/algo/seq/test/test_compatibility.py
+++ maia/algo/seq/test/test_compatibility.py
@@ -1,25 +1,39 @@
 import pytest
-
-import numpy as np
 import os
-from mpi4py import MPI
+import numpy as np
 
 from maia.utils import test_utils as TU
 
-import cmaia
 import maia
 import maia.pytree as PT
 
-
 @pytest.fixture
 def poly_tree_new():
-  filename = os.path.join(TU.mesh_dir,'hex_2_prism_2.yaml')
-  t = maia.io.file_to_dist_tree(filename,MPI.COMM_SELF)
-  maia.algo.dist.convert_elements_to_ngon(t,MPI.COMM_SELF,True)
-  maia.io.distribution_tree.clean_distribution_info(t) # remove distribution info to make it a regular pytree
-  return t
+  return PT.yaml.to_cgns_tree(
+  """
+  Zone Zone_t I4 [[15, 4, 0]]:
+    ZoneType ZoneType_t 'Unstructured':
+    NGON_n Elements_t I4 [22, 0]:
+      ElementRange IndexRange_t I4 [1, 18]:
+      ElementConnectivity DataArray_t:
+        I4 : [1, 6, 9, 4, 6, 11, 14, 9, 3, 5, 10, 8, 8, 10, 15, 13, 1, 2, 7, 6, 2, 3, 8, 7, 6, 7, 12, 11, 7, 8, 13, 12,
+              4, 9, 10, 5, 9, 14, 15, 10, 1, 4, 5, 2, 11, 12, 15, 14, 2, 5, 3, 12, 13, 15, 7, 8, 10, 2, 5, 10, 7, 6, 7,
+              10, 9, 7, 10, 15, 12]
+      ElementStartOffset DataArray_t I4 [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 51, 54, 57, 61, 65, 69]:
+      ParentElements DataArray_t:
+        I4 : [[19, 0], [20, 0], [21, 0], [22, 0], [19, 0], [21, 0], [20, 0], [22, 0], [19, 0], [20, 0], [19, 0], [20, 0],
+              [21, 0], [22, 0], [21, 22], [19, 21], [19, 20], [20, 22]]
+      ParentElementsPosition DataArray_t:
+        I4 : [[5, 0], [5, 0], [2, 0], [2, 0], [2, 0], [1, 0], [2, 0], [1, 0], [4, 0], [4, 0], [1, 0], [6, 0], [4, 0], [5,
+              0], [5, 4], [3, 3], [6, 1], [3, 3]]
+    NFACE_n Elements_t I4 [23, 0]:
+      ElementRange IndexRange_t I4 [19, 22]:
+      ElementConnectivity DataArray_t:
+        I4 : [11, 5, 16, 9, 1, 17, -17, 7, 18, 10, 2, 12, 6, 3, -16, 13, 15, 8, 4, -18, -15, 14]
+      ElementStartOffset DataArray_t I4 [0, 6, 12, 17, 22]:
+  """)
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
+
 def test_enfore_ngon_pe_local(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.enforce_ngon_pe_local(t)
@@ -33,7 +47,6 @@ def test_enfore_ngon_pe_local(poly_tree_new):
   assert (pe == np.array([[1,0],[2,0],[3,0],[4,0],[1,0],[3,0],[2,0],[4,0],[1,0],
                           [2,0],[1,0],[2,0],[3,0],[4,0],[3,4],[1,3],[1,2],[2,4]])).all()
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_new_to_old(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t)
@@ -54,7 +67,6 @@ def test_poly_new_to_old(poly_tree_new):
   assert (nface_ec == np.array([ 6, 11,5,16,9,1,17,    6, 17,7,18,10,2,12,    5, 6,3,16,13,15,    5, 8,4,18,15,14])).all()
 
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_new_to_old_only_interleave(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t,full_onera_compatibility=False)
@@ -74,17 +86,16 @@ def test_poly_new_to_old_only_interleave(poly_tree_new):
                           [20,0],[19,0],[20,0],[21,0],[22,0],[21,22],[19,21],[19,20],[20,22]])).all()
   assert (nface_ec == np.array([ 6, 11,5,16,9,1,17,    6, -17,7,18,10,2,12,    5, 6,3,-16,13,15,    5, 8,4,-18,-15,14])).all()
 
-def test_poly_new_to_old_2d():
-  tree = maia.factory.generate_dist_block(3, "TRI_3", MPI.COMM_SELF)
-  maia.algo.dist.convert_elements_to_ngon(tree, MPI.COMM_SELF)
-  ftree = maia.factory.dist_to_full_tree(tree, MPI.COMM_SELF)
+def test_poly_new_to_old_2d(comm):
+  tree = maia.factory.generate_dist_block(3, "TRI_3", comm)
+  maia.algo.dist.convert_elements_to_ngon(tree, comm)
+  ftree = maia.factory.dist_to_full_tree(tree, comm)
   
   maia.algo.seq.poly_new_to_old(ftree)
   ngon = PT.get_node_from_name(ftree, "NGonElements")
   ngon_ec = PT.get_value(PT.get_child_from_name(ngon,"ElementConnectivity"))
   assert (ngon_ec == np.array([3, 1,2,4, 3, 4,2,5, 3, 5,2,3, 3, 5,3,6, 3, 4,5,7, 3, 7,5,8, 3, 8,5,6, 3, 8,6,9])).all()
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_old_to_new(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t) # Note: we are not testing that!
@@ -110,7 +121,6 @@ def test_poly_old_to_new(poly_tree_new):
   assert (nface_ec  == np.array([1,5,9,11,16,17,    2,7,10,12,18,-17,    3,6,13,15,-16,    4,8,14,-15,-18])).all()
 
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_old_to_new_only_index(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t,full_onera_compatibility=False) # Note: we are not testing that!
@@ -135,7 +145,6 @@ def test_poly_old_to_new_only_index(poly_tree_new):
   assert (nface_eso == np.array([0,6,12,17,22])).all()
   assert (nface_ec  == np.array([11,5,16,9,1,17,    -17,7,18,10,2,12,    6,3,-16,13,15,    8,4,-18,-15,14])).all()
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_old_to_new_no_nface(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t) # Note: we are not testing that!
@@ -156,7 +165,6 @@ def test_poly_old_to_new_no_nface(poly_tree_new):
                                [20,0],[19,0],[20,0],[21,0],[22,0],[21,22],[19,21],[19,20],[20,22]])).all()
 
 
-@pytest.mark.skipif(not cmaia.cpp20_enabled, reason="Require ENABLE_CPP20 compilation flag")
 def test_poly_old_to_new_no_pe(poly_tree_new):
   t = poly_tree_new
   maia.algo.seq.poly_new_to_old(t) # Note: we are not testing that!
